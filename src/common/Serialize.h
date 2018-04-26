2759e55c472acf308dfcaaeb344b32007788d681
slipher <dog964@xemaps.com>
2018-04-26 08:35:23 -0700

    Add #include for memcpy

--- src/common/Serialize.h
+++ src/common/Serialize.h
@@ -35,6 +35,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <map>
 #include <set>
 #include <string>
+#include <string.h>
 #include <unordered_map>
 #include <unordered_set>
 #include <utility>
