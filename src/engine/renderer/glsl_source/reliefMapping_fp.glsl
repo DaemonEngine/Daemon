DaemonEngine/Daemon

commit 071c7890394f2d05c6c869e87ec696b85b014f3e

tr_image: fix heightmap in normalmap detection: heightmap is not anymore a depthmap, fix #233

src/engine/renderer/glsl_source/reliefMapping_fp.glsl
@@ -90,9 +90,6 @@ vec3 NormalInTangentSpace(vec2 texNormal)
 #if defined(r_NormalScale)
 	normal.z *= r_NormalScale;
 #endif
-
-#else // !r_normalMapping
-	normal = vec3(0.0, 0.0, 1.0);
 #endif // r_normalMapping
 
 	return normal;

src/engine/renderer/tr_image.cpp
@@ -1002,52 +1002,30 @@ void R_UploadImage( const byte **dataArray, int numLayers, int numMips,
 	}
 	else
 	{
-		int samples;
-
 		// scan the texture for each channel's max values
 		// and verify if the alpha channel is being used or not
+
 		c = image->width * image->height;
 		scan = dataArray[0];
 
-		samples = 3;
+		// lightmap does not have alpha channel
 
-		// Tr3B: normalmaps have the displacement maps in the alpha channel
-		// samples 3 would cause an opaque alpha channel and odd displacements!
-		if ( image->bits & IF_NORMALMAP )
-		{
-			if ( image->bits & ( IF_DISPLACEMAP | IF_ALPHATEST ) )
-			{
-				samples = 4;
-			}
-			else
-			{
-				samples = 3;
-			}
-		}
-		else if ( image->bits & IF_LIGHTMAP )
-		{
-			samples = 3;
-		}
-		else
+		// normalmap may have the displacement maps in the alpha channel
+		// opaque alpha channel means no displacement, so we can enable
+		// alpha channel everytime it is used, even for normalmap
+
+		internalFormat = GL_RGB8;
+
+		if ( !( image->bits & IF_LIGHTMAP ) )
 		{
 			for ( i = 0; i < c; i++ )
 			{
 				if ( scan[ i * 4 + 3 ] != 255 )
 				{
-					samples = 4;
+					internalFormat = GL_RGBA8;
 					break;
 				}
 			}
-		}
-
-		// select proper internal format
-		if ( samples == 3 )
-		{
-			internalFormat = GL_RGB8;
-		}
-		else if ( samples == 4 )
-		{
-			internalFormat = GL_RGBA8;
 		}
 	}
 
@@ -2837,7 +2815,7 @@ void R_CreateBuiltinImages()
 	tr.blueImage = R_CreateImage( "_blue", ( const byte ** ) &dataPtr,
 				      8, 8, 1, IF_NOPICMIP, filterType_t::FT_LINEAR, wrapTypeEnum_t::WT_REPEAT );
 
-	// generate a default normalmap with a zero heightmap
+	// generate a default normalmap with a fully opaque heightmap (no displacement)
 	for ( x = DEFAULT_SIZE * DEFAULT_SIZE, out = &data[0][0][0]; x; --x, out += 4 )
 	{
 		out[ 0 ] = out[ 1 ] = 128;

commit f4a60e39daf4377b369e58195eb21c1d7d229354

reliefMapping: revert my mistake introduced in 071c789: the flat texture is not read
the glsl code was written to even not read the flat texture
we may even rewrite the CPU part to not bind the flat fallback
at all when r_normalMapping is disabled.

use same value as flat texture instead (0.5, 0.5, 1) as defined in tr_image.cpp
even if (0, 0, 1) will not be that wrong, just to keep things consistent.

the flat texture is still used when normalmapping is enabled but
material does not provide normal map.

src/engine/renderer/glsl_source/reliefMapping_fp.glsl
@@ -90,7 +90,9 @@ vec3 NormalInTangentSpace(vec2 texNormal)
 #if defined(r_NormalScale)
 	normal.z *= r_NormalScale;
 #endif
-#endif // r_normalMapping
+#else // !r_normalMapping
+	normal = vec3(0.5, 0.5, 1.0);
+#endif // !r_normalMapping
 
 	return normal;
 }