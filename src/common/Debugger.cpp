07731573d53a8ae821a84bd67faed0aec57d18b9
Tsarevich Dmitry <dimhotepus@gmail.com>
2020-03-29 19:16:00 +0300

    [check] Fix debugger check
    If the current process is running in the context of a debugger, the return value is nonzero.
    
    See https://docs.microsoft.com/en-us/windows/win32/api/debugapi/nf-debugapi-isdebuggerpresent

--- src/common/Debugger.cpp
+++ src/common/Debugger.cpp
@@ -34,7 +34,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
     namespace Sys {
         bool IsDebuggerAttached() {
-            return IsDebuggerPresent() == TRUE;
+            return IsDebuggerPresent() != FALSE;
         }
     }
 
