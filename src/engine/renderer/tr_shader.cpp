DaemonEngine/Daemon

commit 76cf787f21bb483ede76c4f614fb89aa71581f96

renderer: do not load textures for disabled feature

src/engine/renderer/tr_shader.cpp
@@ -1414,6 +1414,17 @@ static bool LoadMap( shaderStage_t *stage, const char *buffer, const int bundleI
 
 	token = COM_ParseExt2( &buffer_p, false );
 
+	// NOTE: Normal map can ship height map in alpha channel.
+	if ( ( stage->type == stageType_t::ST_NORMALMAP && !r_normalMapping && !r_reliefMapping )
+		|| ( stage->type == stageType_t::ST_HEIGHTMAP && !r_reliefMapping )
+		|| ( stage->type == stageType_t::ST_SPECULARMAP && !r_specularMapping )
+		|| ( stage->type == stageType_t::ST_PHYSICALMAP && !r_physicalMapping )
+		|| ( stage->type == stageType_t::ST_GLOWMAP && !r_glowMapping )
+		|| ( stage->type == stageType_t::ST_REFLECTIONMAP && !r_reflectionMapping ) )
+	{
+		return true;
+	}
+
 	if ( !Q_stricmp( token, "$whiteimage" ) || !Q_stricmp( token, "$white" ) || !Q_stricmp( token, "_white" ) ||
 	     !Q_stricmp( token, "*white" ) )
 	{

commit 436f95fdde1069a5e55ce1f2e0d4c28e27162723

tr_shader: test against cvar value instead of pointer, fix #376, ref #358, #377
- fixes #376 issue introduced in 76cf787f21bb483ede76c4f614fb89aa71581f96
- makes #358 working as expected
- see also #377 for hardening the fix for #376

src/engine/renderer/tr_shader.cpp
@@ -1415,12 +1415,12 @@ static bool LoadMap( shaderStage_t *stage, const char *buffer, const int bundleI
 	token = COM_ParseExt2( &buffer_p, false );
 
 	// NOTE: Normal map can ship height map in alpha channel.
-	if ( ( stage->type == stageType_t::ST_NORMALMAP && !r_normalMapping && !r_reliefMapping )
-		|| ( stage->type == stageType_t::ST_HEIGHTMAP && !r_reliefMapping )
-		|| ( stage->type == stageType_t::ST_SPECULARMAP && !r_specularMapping )
-		|| ( stage->type == stageType_t::ST_PHYSICALMAP && !r_physicalMapping )
-		|| ( stage->type == stageType_t::ST_GLOWMAP && !r_glowMapping )
-		|| ( stage->type == stageType_t::ST_REFLECTIONMAP && !r_reflectionMapping ) )
+	if ( ( stage->type == stageType_t::ST_NORMALMAP && !r_normalMapping->integer && !r_reliefMapping->integer )
+		|| ( stage->type == stageType_t::ST_HEIGHTMAP && !r_reliefMapping->integer )
+		|| ( stage->type == stageType_t::ST_SPECULARMAP && !r_specularMapping->integer )
+		|| ( stage->type == stageType_t::ST_PHYSICALMAP && !r_physicalMapping->integer )
+		|| ( stage->type == stageType_t::ST_GLOWMAP && !r_glowMapping->integer )
+		|| ( stage->type == stageType_t::ST_REFLECTIONMAP && !r_reflectionMapping->integer ) )
 	{
 		return true;
 	}