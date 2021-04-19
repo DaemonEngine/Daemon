90dbf4264aa1995a2e962a76e4a588a7336a5e75
peoro <peoro.noob@gmail.com>
2021-04-19 17:55:08 +0200

    Namespace aware autocompletion. Resolves DaemonEngine/Daemon#449

--- src/engine/framework/ConsoleField.cpp
+++ src/engine/framework/ConsoleField.cpp
@@ -129,9 +129,57 @@ namespace Console {
         //Print the matches if it is ambiguous
         if (candidates.size() >= 2) {
             Log::CommandInteractionMessage(Str::Format("^3-> ^*%s", Str::UTF32To8(GetText())));
-            for (const auto& candidate : candidates) {
+            
+            auto showCandidate = [&]( Cmd::CompletionItem candidate ) {
                 std::string filler(maxCandidateLength - candidate.first.length(), ' ');
                 Log::CommandInteractionMessage(Str::Format("   %s%s %s", candidate.first, filler, candidate.second));
+            };
+            
+            // we only group candidates by namespace for commands, not for arguments
+            if( argNum > 1 ) {
+                // print everything
+                for (const auto& candidate : candidates) {
+                    showCandidate( candidate );
+                }
+            }
+            else {
+                using CompletionIterator = Cmd::CompletionResult::iterator; 
+                
+                // for every candidate (`it`)
+                // we look for the last candidate that shares at least a namespace not included in the prefix (`jt`)
+                // we can do this easily since the candidates are sorted
+                for( CompletionIterator it = candidates.begin(); it != candidates.end(); ++it ) {
+                    // looking for `jt`: the last candidate that shares with `it` a namespace not included in the prefix
+                    CompletionIterator jt;
+                    int nsLen = 0; // the namespace shared by `it` and `jt` is `nsLen` characters
+                    
+                    for( jt = it+1; jt != candidates.end(); ++jt ) {
+                        int commonPrefixLen = Str::LongestPrefixSize( it->first, jt->first );
+                        int commonNSLen = it->first.rfind( '.', commonPrefixLen );
+                        
+                        // let's stop looking at the first candidate that doesn't share a namespace with `it`
+                        if( commonNSLen == commonPrefixLen || commonNSLen < prefixSize ) {
+                            break;
+                        }
+                        
+                        nsLen = commonNSLen;
+                    }
+                    
+                    // `jt` is the first candidate after `it` which doesn't share a namespace with it, take the previous one
+                    -- jt;
+                    
+                    // if `it` doesn't share a namespace with any other candidate, print it entirely
+                    if( it == jt ) {
+                        showCandidate( *it );
+                    }
+                    // else show the namespace and the amount of items inside of it
+                    // then skip all the elements inside such namespace
+                    else {
+                        std::string ns( it->first, 0, nsLen );
+                        Log::CommandInteractionMessage(Str::Format("   %s.{x%i}", ns, jt-it+1));
+                        it = jt;
+                    }
+                }
             }
         }
     }
