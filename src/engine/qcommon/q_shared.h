42fdad6a1b1fc41073458f0f380196c87a2403ba
Morel Bérenger <berengermorel76@gmail.com>
2021-04-28 15:33:51 +0200

    moved code used only once from a header to the place really using it

--- src/engine/qcommon/q_shared.h
+++ src/engine/qcommon/q_shared.h
@@ -465,16 +465,6 @@ void         ByteToDir( int b, vec3_t dir );
 
 	int VectorCompare( const vec3_t v1, const vec3_t v2 );
 
-	inline int Vector4Compare( const vec4_t v1, const vec4_t v2 )
-	{
-		if ( v1[ 0 ] != v2[ 0 ] || v1[ 1 ] != v2[ 1 ] || v1[ 2 ] != v2[ 2 ] || v1[ 3 ] != v2[ 3 ] )
-		{
-			return 0;
-		}
-
-		return 1;
-	}
-
 	inline void VectorLerp( const vec3_t from, const vec3_t to, float frac, vec3_t out )
 	{
 		out[ 0 ] = from[ 0 ] + ( ( to[ 0 ] - from[ 0 ] ) * frac );
