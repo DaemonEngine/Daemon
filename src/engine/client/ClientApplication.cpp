7634af035e74ab7b2bb44147fb7c7ff4ca8dd337
Thomas Debesse <dev@illwieckz.net>
2022-03-17 01:30:12 +0100

    system: also forward -connect, fix #607

--- src/engine/client/ClientApplication.cpp
+++ src/engine/client/ClientApplication.cpp
@@ -70,16 +70,12 @@ class ClientApplication : public Application {
 	        }
         }
 
-        void Initialize(Str::StringRef uri) override {
+        void Initialize() override {
             Hunk_Init();
 
             Com_Init();
 
             CL_StartHunkUsers();
-
-            if (!uri.empty()) {
-                Cmd::BufferCommandTextAfter(std::string("connect ") + Cmd::Escape(uri));
-            }
         }
 
         void Frame() override {
