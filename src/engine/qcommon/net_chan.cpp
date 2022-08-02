b076c39fbedf5e4ed44da3d011a4a4558f3ed0bf
DaVinciCode2 <nomestpersonne@gmail.com>
2022-08-02 21:58:36 +0200

    fix for signedness problem with network ports > 2 power 15

--- src/engine/qcommon/net_chan.cpp
+++ src/engine/qcommon/net_chan.cpp
@@ -650,12 +650,12 @@ int NET_StringToAdr( const char *s, netadr_t *a, netadrtype_t family )
 
 	if ( port )
 	{
-		a->port = BigShort( ( short ) atoi( port ) );
+		a->port = UBigShort( ( unsigned short ) atoi( port ) );
 		return 1;
 	}
 	else
 	{
-		a->port = BigShort( PORT_SERVER );
+		a->port = UBigShort( PORT_SERVER );
 		return 2;
 	}
 }
