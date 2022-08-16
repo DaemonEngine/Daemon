87e9fb41fdb94bc51a53d03aa0518fc64f3fc052
Thomas Debesse <dev@illwieckz.net>
2022-08-16 12:42:44 +0200

    tr_shader: add some missing cases for PBR stageType in some switches

--- src/engine/renderer/tr_shade.cpp
+++ src/engine/renderer/tr_shade.cpp
@@ -2942,6 +2942,7 @@ void Tess_StageIteratorDepthFill()
 
 			case stageType_t::ST_DIFFUSEMAP:
 			case stageType_t::ST_COLLAPSE_lighting_PHONG:
+			case stageType_t::ST_COLLAPSE_lighting_PBR:
 				{
 					Render_depthFill( stage );
 					break;
@@ -3022,6 +3023,7 @@ void Tess_StageIteratorShadowFill()
 			case stageType_t::ST_LIGHTMAP:
 			case stageType_t::ST_DIFFUSEMAP:
 			case stageType_t::ST_COLLAPSE_lighting_PHONG:
+			case stageType_t::ST_COLLAPSE_lighting_PBR:
 				{
 					Render_shadowFill( stage );
 					break;
