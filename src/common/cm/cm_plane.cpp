e60b2735e1942d318dd69d772d1b9413cbb65163
dolcetriade <vcelestialragev@gmail.com>
2022-08-28 01:02:25 -0700

    cm: Silence invalid bevel warnings.
    ioq3 has it as a debug print. ET:Legacy has it commented it out all together. It seems this check doesn't apply anymore.

--- src/common/cm/cm_plane.cpp
+++ src/common/cm/cm_plane.cpp
@@ -622,7 +622,7 @@ void CM_AddFacetBevels( cFacet_t *facet )
 
 					if ( !w2 )
 					{
-						cmLog.Warn( "CM_AddFacetBevels... invalid bevel" );
+						cmLog.Debug( "CM_AddFacetBevels... invalid bevel" );
 						continue;
 					}
 					else
