37f9828b18381a11724bf30d979cb0a7ba2c2693
Thomas Debesse <dev@illwieckz.net>
2022-09-04 18:06:44 +0000

    Update version to 0.53.2

--- src/common/Defs.h
+++ src/common/Defs.h
@@ -37,7 +37,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /** No case, No spaces */
 #define PRODUCT_NAME_LOWER  "unvanquished"
 
-#define PRODUCT_VERSION     "0.53.0"
+#define PRODUCT_VERSION     "0.53.2"
 
 /** Default base package */
 #define DEFAULT_BASE_PAK    PRODUCT_NAME_LOWER
