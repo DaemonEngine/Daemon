7ea99b036f229b59c4e1470190f95fb3df664340
slipher <slipher@protonmail.com>
2023-01-13 05:55:53 -0600

    Remove useless null check in CL_SetServerInfo

--- src/engine/client/cl_main.cpp
+++ src/engine/client/cl_main.cpp
@@ -3018,26 +3018,23 @@ void CL_Shutdown()
 
 static void CL_SetServerInfo( serverInfo_t *server, const char *info, int ping )
 {
-	if ( server )
+	if ( info )
 	{
-		if ( info )
-		{
-			server->clients = atoi( Info_ValueForKey( info, "clients" ) );
-			server->bots = atoi( Info_ValueForKey( info, "bots" ) );
-			Q_strncpyz( server->hostName, Info_ValueForKey( info, "hostname" ), MAX_NAME_LENGTH );
-			server->load = atoi( Info_ValueForKey( info, "serverload" ) );
-			Q_strncpyz( server->mapName, Info_ValueForKey( info, "mapname" ), MAX_NAME_LENGTH );
-			server->maxClients = atoi( Info_ValueForKey( info, "sv_maxclients" ) );
-			Q_strncpyz( server->game, Info_ValueForKey( info, "game" ), MAX_NAME_LENGTH );
-			server->netType = Util::enum_cast<netadrtype_t>(atoi(Info_ValueForKey(info, "nettype")));
-			server->minPing = atoi( Info_ValueForKey( info, "minping" ) );
-			server->maxPing = atoi( Info_ValueForKey( info, "maxping" ) );
-			server->needpass = atoi( Info_ValueForKey( info, "g_needpass" ) );   // NERVE - SMF
-			Q_strncpyz( server->gameName, Info_ValueForKey( info, "gamename" ), MAX_NAME_LENGTH );   // Arnout
-		}
-
-		server->ping = ping;
+		server->clients = atoi( Info_ValueForKey( info, "clients" ) );
+		server->bots = atoi( Info_ValueForKey( info, "bots" ) );
+		Q_strncpyz( server->hostName, Info_ValueForKey( info, "hostname" ), MAX_NAME_LENGTH );
+		server->load = atoi( Info_ValueForKey( info, "serverload" ) );
+		Q_strncpyz( server->mapName, Info_ValueForKey( info, "mapname" ), MAX_NAME_LENGTH );
+		server->maxClients = atoi( Info_ValueForKey( info, "sv_maxclients" ) );
+		Q_strncpyz( server->game, Info_ValueForKey( info, "game" ), MAX_NAME_LENGTH );
+		server->netType = Util::enum_cast<netadrtype_t>(atoi(Info_ValueForKey(info, "nettype")));
+		server->minPing = atoi( Info_ValueForKey( info, "minping" ) );
+		server->maxPing = atoi( Info_ValueForKey( info, "maxping" ) );
+		server->needpass = atoi( Info_ValueForKey( info, "g_needpass" ) );   // NERVE - SMF
+		Q_strncpyz( server->gameName, Info_ValueForKey( info, "gamename" ), MAX_NAME_LENGTH );   // Arnout
 	}
+
+	server->ping = ping;
 }
 
 static void CL_SetServerInfoByAddress( const netadr_t& from, const char *info, int ping )
