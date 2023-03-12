d2b4700a0c0ffb0aa37d7d855454b6403358da78
Thomas Debesse <dev@illwieckz.net>
2023-03-12 19:04:52 +0100

    cmake,DaemonArchitecture: add armel architecture for soft float 32-bit ARM ABI
    Mainstream 32-bit ARM Linux distributions use "hard" float ABI.
    Example: Debian armhf
    
    Some (less used) 32-bit ARM Linux distributions use "soft" float ABI.
    Example: Debian armel
    
    Android 32-bit ARM distributions use older "soft" float ABI,
    like Debian armel. This is very likely to maintain compatibility
    over a very large range of hardware, especially since VFP was still
    optional in Armv7-A which is still widely used.
    
    On Linux, armhf is now preferred over armel on recent hardware,
    starting with Armv7-A with VFP. Before that, armel was used on
    older hardware up to Armv7-A without VFP, it should still works.
    Debian Buster provides both armhf and armel distributions.
    
    By building the software with the compatible "softfp" ABI variant it
    is allowed to do hardware floats using the VFP while keeping compatibility
    with "soft" libraries and then armel systems at the cost of some overhead.
    
    Distributions favorising performance will prefer armhf.
    Distributions favorising compatibility will prefer armel.

--- tools/DaemonArchitecture/DaemonArchitecture.cpp
+++ tools/DaemonArchitecture/DaemonArchitecture.cpp
@@ -91,8 +91,11 @@ platform variants we cannot support anyway. */
 #elif defined(Q_PROCESSOR_ARM_64)
 	#pragma message("DAEMON_ARCH_arm64")
 
-#elif defined(Q_PROCESSOR_ARM_32) && Q_PROCESSOR_ARM == 7
+#elif defined(Q_PROCESSOR_ARM_32) && Q_PROCESSOR_ARM >= 7 && defined(__ARM_PCS_VFP)
 	#pragma message("DAEMON_ARCH_armhf")
+
+#elif defined(Q_PROCESSOR_ARM_32) && Q_PROCESSOR_ARM >= 6 && !defined(__ARM_PCS_VFP)
+	#pragma message("DAEMON_ARCH_armel")
 
 /* Devices like:
  - Raptor Computing Systems Talos, Blackbird… */
