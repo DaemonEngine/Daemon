84a3f1014ad9f9457a089b8eea6fc71264c65ac8
slipher <slipher@protonmail.com>
2024-03-23 12:12:06 -0500

    Delete unused old-style cvars

--- src/engine/client/cl_main.cpp
+++ src/engine/client/cl_main.cpp
@@ -75,9 +75,7 @@ cvar_t *cl_timeNudge;
 cvar_t *cl_showTimeDelta;
 
 cvar_t *cl_shownet = nullptr; // NERVE - SMF - This is referenced in msg.c and we need to make sure it is nullptr
-cvar_t *cl_shownuments; // DHM - Nerve
 cvar_t *cl_showSend;
-cvar_t *cl_showServerCommands; // NERVE - SMF
 
 Cvar::Cvar<bool> cvar_demo_status_isrecording(
     "demo.status.isrecording",
@@ -127,7 +125,6 @@ cvar_t *cl_autorecord;
 
 cvar_t *cl_allowDownload;
 
-cvar_t                 *cl_packetloss; //bani
 cvar_t                 *cl_packetdelay; //bani
 
 cvar_t                 *cl_consoleFont;
@@ -146,8 +143,6 @@ cvar_t             *cl_aviMotionJpeg;
 // XreaL END
 
 cvar_t             *cl_rate;
-
-cvar_t             *cl_cgameSyscallStats;
 
 clientActive_t     cl;
 clientConnection_t clc;
@@ -2356,8 +2351,6 @@ void CL_Init()
 
 	cl_timeNudge = Cvar_Get( "cl_timeNudge", "0", CVAR_TEMP );
 	cl_shownet = Cvar_Get( "cl_shownet", "0", CVAR_TEMP );
-	cl_shownuments = Cvar_Get( "cl_shownuments", "0", CVAR_TEMP );
-	cl_showServerCommands = Cvar_Get( "cl_showServerCommands", "0", 0 );
 	cl_showSend = Cvar_Get( "cl_showSend", "0", CVAR_TEMP );
 	cl_showTimeDelta = Cvar_Get( "cl_showTimeDelta", "0", CVAR_TEMP );
 	cl_activeAction = Cvar_Get( "activeAction", "", CVAR_TEMP );
@@ -2421,7 +2414,6 @@ void CL_Init()
 	cl_altTab = Cvar_Get( "cl_altTab", "1", 0 );
 
 	//bani
-	cl_packetloss = Cvar_Get( "cl_packetloss", "0", CVAR_CHEAT );
 	cl_packetdelay = Cvar_Get( "cl_packetdelay", "0", CVAR_CHEAT );
 
 	// userinfo
@@ -2435,8 +2427,6 @@ void CL_Init()
 	cl_useMumble = Cvar_Get( "cl_useMumble", "0",  CVAR_LATCH );
 	cl_mumbleScale = Cvar_Get( "cl_mumbleScale", "0.0254", 0 );
 #endif
-
-	cl_cgameSyscallStats = Cvar_Get( "cl_cgameSyscallStats", "0", 0 );
 
 	//
 	// register our commands
