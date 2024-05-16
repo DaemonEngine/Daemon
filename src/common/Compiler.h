bfa7b90408f69f09a3153b382305cb5b75c59d0e
slipher <slipher@protonmail.com>
2024-05-16 14:56:51 -0300

    Disable MSVC warning about struct holes

--- src/common/Compiler.h
+++ src/common/Compiler.h
@@ -141,6 +141,8 @@ inline int CountTrailingZeroes(unsigned long long x) { return __builtin_ctzll(x)
 #pragma warning(disable : 4244) // 'XXX': conversion from 'YYY' to 'ZZZ', possible loss of data
 #pragma warning(disable : 4267) // 'initializing' : conversion from 'size_t' to 'int', possible loss of data
 
+#pragma warning(disable : 4324) // 'refBone_t': structure was padded due to alignment specifier
+
 #pragma warning(disable : 4458) // declaration of 'XXX' hides class member
 #pragma warning(disable : 4459) // declaration of 'XXX' hides global declaration
 
