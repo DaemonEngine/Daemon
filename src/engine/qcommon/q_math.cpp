15299f63161e441262bd3626fa4bb7169de1cbb4
slipher <slipher@protonmail.com>
2024-06-06 18:16:46 -0500

    Convert most vector macros into functions
    This is good because it prevents multiple evaluation of the arguments.
    As a live example of this bug, the CG_CalculateTimeFrac inside a
    VectorMA in CG_RenderParticle in Unvanquished/src/cgame/cg_particles.cpp
    would be evaluated 3 times.
    
    Also it may be useful to copy a glm::vec3 returned from a function into
    a vec3_t without needing a temporary variable.

--- src/engine/qcommon/q_math.cpp
+++ src/engine/qcommon/q_math.cpp
@@ -36,6 +36,9 @@
 
 #include "q_shared.h"
 
+#define DotProduct4(x, y)            (( x )[ 0 ] * ( y )[ 0 ] + ( x )[ 1 ] * ( y )[ 1 ] + ( x )[ 2 ] * ( y )[ 2 ] + ( x )[ 3 ] * ( y )[ 3 ] )
+#define VectorMultiply( a,b,c )      ( ( c )[ 0 ] = ( a )[ 0 ] * ( b )[ 0 ],( c )[ 1 ] = ( a )[ 1 ] * ( b )[ 1 ],( c )[ 2 ] = ( a )[ 2 ] * ( b )[ 2 ] )
+
 const vec3_t vec3_origin = { 0, 0, 0 };
 
 const vec3_t axisDefault[ 3 ] = { { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
