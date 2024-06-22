DaemonEngine/Daemon

commit 3b2dda7bb71d5bb4716a745fd44415dba7a9a6f4

IQM loader: remove redundant weight correction
When populating blendWeights, a calculation like weights[0] = 255 -
weights[1] - weights[2] - weights[3] was done so all-0 weights would
have been already fixed.

src/engine/renderer/tr_model_iqm.cpp
@@ -780,12 +780,6 @@ bool R_LoadIQModel( model_t *mod, const void *buffer, int filesize,
 
 			weightbuf = (float *)ri.Hunk_AllocateTempMemory( sizeof(vec4_t) * IQModel->num_vertexes );
 			for(int i = 0; i < IQModel->num_vertexes; i++ ) {
-				if( IQModel->blendWeights[ 4 * i + 0 ] == 0 &&
-				    IQModel->blendWeights[ 4 * i + 1 ] == 0 &&
-				    IQModel->blendWeights[ 4 * i + 2 ] == 0 &&
-				    IQModel->blendWeights[ 4 * i + 3 ] == 0 )
-					IQModel->blendWeights[ 4 * i + 0 ] = 255;
-
 				weightbuf[ 4 * i + 0 ] = weightscale * IQModel->blendWeights[ 4 * i + 0 ];
 				weightbuf[ 4 * i + 1 ] = weightscale * IQModel->blendWeights[ 4 * i + 1 ];
 				weightbuf[ 4 * i + 2 ] = weightscale * IQModel->blendWeights[ 4 * i + 2 ];
@@ -834,16 +828,6 @@ bool R_LoadIQModel( model_t *mod, const void *buffer, int filesize,
 	} else {
 		vbo = nullptr;
 		ibo = nullptr;
-
-		if( IQModel->blendWeights ) {
-			for( int i = 0; i < IQModel->num_vertexes; i++ ) {
-				if( IQModel->blendWeights[ 4 * i + 0 ] == 0 &&
-				    IQModel->blendWeights[ 4 * i + 1 ] == 0 &&
-				    IQModel->blendWeights[ 4 * i + 2 ] == 0 &&
-				    IQModel->blendWeights[ 4 * i + 3 ] == 0 )
-					IQModel->blendWeights[ 4 * i + 0 ] = 255;
-			}
-		}
 	}
 
 	// register shaders

commit c2c0f55effb7622372a40ae9632c7ffc7afb205b

Fix regression with IQM without weights
A regression in 3b2dda7bb71d5bb4716a745fd44415dba7a9a6f4
which affected IQMs without a weights section
caused alien missiles to be invisible.

src/engine/renderer/tr_model_iqm.cpp
@@ -461,7 +461,6 @@ bool R_LoadIQModel( model_t *mod, const void *buffer, int filesize,
 	VBO_t                   *vbo;
 	IBO_t                   *ibo;
 	void                    *ptr;
-	u8vec4_t                *weights;
 
 	if( !LoadIQMFile( buffer, filesize, mod_name, &len_names ) ) {
 		return false;
@@ -681,6 +680,8 @@ bool R_LoadIQModel( model_t *mod, const void *buffer, int filesize,
 		}
 	}
 
+	u8vec4_t *weights = nullptr;
+
 	// copy vertexarrays and indexes
 	vertexarray = static_cast<iqmVertexArray_t*>( IQMPtr( header, header->ofs_vertexarrays ) );
 	for(unsigned i = 0; i < header->num_vertexarrays; i++, vertexarray++ ) {
@@ -746,6 +747,14 @@ bool R_LoadIQModel( model_t *mod, const void *buffer, int filesize,
 				    IQMPtr( header, vertexarray->offset ),
 				    n * sizeof(byte) );
 			break;
+		}
+	}
+
+	if ( !weights )
+	{
+		for ( unsigned j = 0; j < header->num_vertexes; j++ )
+		{
+			IQModel->blendWeights[ 4 * j + 0 ] = 255;
 		}
 	}
 