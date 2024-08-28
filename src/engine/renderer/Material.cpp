302f72c86948aecaf662f328ae56ae0e2455d25d
slipher <slipher@protonmail.com>
2024-08-28 13:50:06 -0500

    Unused variable MaterialSystem::currentView

--- src/engine/renderer/Material.cpp
+++ src/engine/renderer/Material.cpp
@@ -1944,9 +1944,6 @@ void MaterialSystem::EndFrame() {
 	if ( nextFrame >= MAX_FRAMES ) {
 		nextFrame = 0;
 	}
-
-	currentView = 0;
-	return;
 }
 
 void MaterialSystem::GeneratePortalBoundingSpheres() {
