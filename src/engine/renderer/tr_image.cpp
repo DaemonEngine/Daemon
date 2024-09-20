DaemonEngine/Daemon

commit 7e7cb4f43e4fabdf54b7b3830e01ee362665fe3f

Use float functions and float litterals for performance

src/engine/renderer/tr_image.cpp
@@ -116,7 +116,7 @@ void GL_TextureMode( const char *string )
 		{
 			Cvar_Set( "r_ext_texture_filter_anisotropic", va( "%f", glConfig2.maxTextureAnisotropy ) );
 		}
-		else if ( r_ext_texture_filter_anisotropic->value < 1.0 )
+		else if ( r_ext_texture_filter_anisotropic->value < 1.0f )
 		{
 			Cvar_Set( "r_ext_texture_filter_anisotropic", "1.0" );
 		}
@@ -2215,19 +2215,19 @@ float R_FogFactor( float s, float t )
 {
 	float d;
 
-	s -= 1.0 / 512;
+	s -= 1.0f / 512.0f;
 
 	if ( s < 0 )
 	{
 		return 0;
 	}
 
-	if ( t < 1.0 / 32 )
+	if ( t < 1.0f / 32.0f )
 	{
 		return 0;
 	}
 
-	if ( t < 31.0 / 32 )
+	if ( t < 31.0f / 320.0f )
 	{
 		s *= ( t - 1.0f / 32.0f ) / ( 30.0f / 32.0f );
 	}
@@ -2235,9 +2235,9 @@ float R_FogFactor( float s, float t )
 	// we need to leave a lot of clamp range
 	s *= 8;
 
-	if ( s > 1.0 )
+	if ( s > 1.0f )
 	{
-		s = 1.0;
+		s = 1.0f;
 	}
 
 	d = tr.fogTable[( int )( s * ( FOG_TABLE_SIZE - 1 ) ) ];
@@ -2347,9 +2347,9 @@ static void R_CreateRandomNormalsImage()
 			float  r, angle;
 
 			r = random();
-			angle = 2.0 * M_PI * r; // / 360.0;
+			angle = 2.0f * M_PI * r; // / 360.0f;
 
-			VectorSet( n, cos( angle ), sin( angle ), r );
+			VectorSet( n, cosf( angle ), sinf( angle ), r );
 			VectorNormalize( n );
 
 			ptr[ 0 ] = ( byte )( 128 + 127 * n[ 0 ] );

src/common/cm/cm_plane.cpp
@@ -102,8 +102,8 @@ bool CM_PlaneFromPoints( vec4_t plane, const vec3_t a, const vec3_t b, const vec
 	return true;
 }
 
-static const double NORMAL_EPSILON    = 0.0001;
-static const double DIST_EPSILON      = 0.02;
+static constexpr float NORMAL_EPSILON    = 0.0001f;
+static constexpr float DIST_EPSILON      = 0.02f;
 
 /*
 ==================
@@ -114,9 +114,9 @@ int CM_PlaneEqual( cPlane_t *p, float plane[ 4 ], bool *flipped )
 {
 	float invplane[ 4 ];
 
-	if ( fabs( p->plane[ 0 ] - plane[ 0 ] ) < NORMAL_EPSILON
-	     && fabs( p->plane[ 1 ] - plane[ 1 ] ) < NORMAL_EPSILON
-	     && fabs( p->plane[ 2 ] - plane[ 2 ] ) < NORMAL_EPSILON && fabs( p->plane[ 3 ] - plane[ 3 ] ) < DIST_EPSILON )
+	if ( fabsf( p->plane[ 0 ] - plane[ 0 ] ) < NORMAL_EPSILON
+	     && fabsf( p->plane[ 1 ] - plane[ 1 ] ) < NORMAL_EPSILON
+	     && fabsf( p->plane[ 2 ] - plane[ 2 ] ) < NORMAL_EPSILON && fabsf( p->plane[ 3 ] - plane[ 3 ] ) < DIST_EPSILON )
 	{
 		*flipped = false;
 		return true;
@@ -125,9 +125,9 @@ int CM_PlaneEqual( cPlane_t *p, float plane[ 4 ], bool *flipped )
 	VectorNegate( plane, invplane );
 	invplane[ 3 ] = -plane[ 3 ];
 
-	if ( fabs( p->plane[ 0 ] - invplane[ 0 ] ) < NORMAL_EPSILON
-	     && fabs( p->plane[ 1 ] - invplane[ 1 ] ) < NORMAL_EPSILON
-	     && fabs( p->plane[ 2 ] - invplane[ 2 ] ) < NORMAL_EPSILON && fabs( p->plane[ 3 ] - invplane[ 3 ] ) < DIST_EPSILON )
+	if ( fabsf( p->plane[ 0 ] - invplane[ 0 ] ) < NORMAL_EPSILON
+	     && fabsf( p->plane[ 1 ] - invplane[ 1 ] ) < NORMAL_EPSILON
+	     && fabsf( p->plane[ 2 ] - invplane[ 2 ] ) < NORMAL_EPSILON && fabsf( p->plane[ 3 ] - invplane[ 3 ] ) < DIST_EPSILON )
 	{
 		*flipped = true;
 		return true;
@@ -147,14 +147,14 @@ void CM_SnapVector( vec3_t normal )
 
 	for ( i = 0; i < 3; i++ )
 	{
-		if ( fabs( normal[ i ] - 1 ) < NORMAL_EPSILON )
+		if ( fabsf( normal[ i ] - 1 ) < NORMAL_EPSILON )
 		{
 			VectorClear( normal );
 			normal[ i ] = 1;
 			break;
 		}
 
-		if ( fabs( normal[ i ] - -1 ) < NORMAL_EPSILON )
+		if ( fabsf( normal[ i ] - -1 ) < NORMAL_EPSILON )
 		{
 			VectorClear( normal );
 			normal[ i ] = -1;
@@ -174,7 +174,7 @@ static long CM_GenerateHashValue( vec4_t plane )
 {
 	long hash;
 
-	hash = ( int ) fabs( plane[ 3 ] );
+	hash = ( int ) fabsf( plane[ 3 ] );
 	hash &= ( PLANE_HASHES - 1 );
 
 	return hash;

src/common/cm/cm_trace.cpp
@@ -339,7 +339,7 @@ static bool CM_PositionTestInSurfaceCollide( traceWork_t *tw, const cSurfaceColl
 			{
 				// NOTE: this works even though the plane might be flipped because the bbox is centered
 				offset = DotProduct( tw->offsets[ planes->signbits ], plane );
-				plane[ 3 ] += fabs( offset );
+				plane[ 3 ] += fabsf( offset );
 				VectorCopy( tw->start, startp );
 			}
 
@@ -851,8 +851,8 @@ void CM_TraceThroughSurfaceCollide( traceWork_t *tw, const cSurfaceCollide_t *sc
 
 	for ( i = 0, facet = sc->facets; i < sc->numFacets; i++, facet++ )
 	{
-		enterFrac = -1.0;
-		leaveFrac = 1.0;
+		enterFrac = -1.0f;
+		leaveFrac = 1.0f;
 		hitnum = -1;
 
 		planes = &sc->planes[ facet->surfacePlane ];
@@ -934,7 +934,7 @@ void CM_TraceThroughSurfaceCollide( traceWork_t *tw, const cSurfaceCollide_t *sc
 			{
 				// NOTE: this works even though the plane might be flipped because the bbox is centered
 				offset = DotProduct( tw->offsets[ planes->signbits ], plane );
-				plane[ 3 ] += fabs( offset );
+				plane[ 3 ] += fabsf( offset );
 				VectorCopy( tw->start, startp );
 				VectorCopy( tw->end, endp );
 			}
@@ -1031,8 +1031,8 @@ void CM_TraceThroughBrush( traceWork_t *tw, cbrush_t *brush )
 	vec3_t       startp;
 	vec3_t       endp;
 
-	enterFrac = -1.0;
-	leaveFrac = 1.0;
+	enterFrac = -1.0f;
+	leaveFrac = 1.0f;
 	clipplane = nullptr;
 
 	if ( !brush->numsides )
@@ -1994,14 +1994,14 @@ static void CM_TraceThroughTree( traceWork_t *tw, int num, float p1f, float p2f,
 	// put the crosspoint SURFACE_CLIP_EPSILON pixels on the near side
 	if ( t1 < t2 )
 	{
-		idist = 1.0 / ( t1 - t2 );
+		idist = 1.0f / ( t1 - t2 );
 		side = 1;
 		frac2 = ( t1 + offset + SURFACE_CLIP_EPSILON ) * idist;
 		frac = ( t1 - offset + SURFACE_CLIP_EPSILON ) * idist;
 	}
 	else if ( t1 > t2 )
 	{
-		idist = 1.0 / ( t1 - t2 );
+		idist = 1.0f / ( t1 - t2 );
 		side = 0;
 		frac2 = ( t1 - offset - SURFACE_CLIP_EPSILON ) * idist;
 		frac = ( t1 + offset + SURFACE_CLIP_EPSILON ) * idist;
@@ -2170,13 +2170,13 @@ static void CM_Trace( trace_t *results, const vec3_t start, const vec3_t end, co
 		{
 			if ( tw.start[ i ] < tw.end[ i ] )
 			{
-				tw.bounds[ 0 ][ i ] = tw.start[ i ] - fabs( tw.sphere.offset[ i ] ) - tw.sphere.radius;
-				tw.bounds[ 1 ][ i ] = tw.end[ i ] + fabs( tw.sphere.offset[ i ] ) + tw.sphere.radius;
+				tw.bounds[ 0 ][ i ] = tw.start[ i ] - fabsf( tw.sphere.offset[ i ] ) - tw.sphere.radius;
+				tw.bounds[ 1 ][ i ] = tw.end[ i ] + fabsf( tw.sphere.offset[ i ] ) + tw.sphere.radius;
 			}
 			else
 			{
-				tw.bounds[ 0 ][ i ] = tw.end[ i ] - fabs( tw.sphere.offset[ i ] ) - tw.sphere.radius;
-				tw.bounds[ 1 ][ i ] = tw.start[ i ] + fabs( tw.sphere.offset[ i ] ) + tw.sphere.radius;
+				tw.bounds[ 0 ][ i ] = tw.end[ i ] - fabsf( tw.sphere.offset[ i ] ) - tw.sphere.radius;
+				tw.bounds[ 1 ][ i ] = tw.start[ i ] + fabsf( tw.sphere.offset[ i ] ) + tw.sphere.radius;
 			}
 		}
 	}
@@ -2418,7 +2418,7 @@ void CM_TransformedBoxTrace( trace_t *results, const vec3_t start, const vec3_t 
 			  brushmask, skipmask, type, &sphere );
 
 	// if the bmodel was rotated and there was a collision
-	if ( rotated && trace.fraction != 1.0 )
+	if ( rotated && trace.fraction != 1.0f )
 	{
 		// rotation of bmodel collision plane
 		TransposeMatrix( matrix, transpose );

src/engine/client/cl_console.cpp
@@ -1014,7 +1014,7 @@ void Con_UpdateConsoleState()
 	/*
 	 * calculate current console height
 	 */
-	consoleState.height = con_height->integer * 0.01 * (cls.glconfig.vidHeight
+	consoleState.height = con_height->integer * 0.01f * (cls.glconfig.vidHeight
 						- consoleState.margin.top - consoleState.margin.bottom
 						- consoleState.border.top - consoleState.border.bottom
 						);
@@ -1131,7 +1131,7 @@ void Con_RunConsole()
 
 	if ( !consoleState.isOpened && consoleState.currentAnimationFraction >= 0.0f )
 	{
-		consoleState.currentAnimationFraction -= con_animationSpeed->value * cls.realFrametime * 0.001;
+		consoleState.currentAnimationFraction -= con_animationSpeed->value * cls.realFrametime * 0.001f;
 
 		if ( consoleState.currentAnimationFraction <= 0.0f  || con_animationType->integer == ANIMATION_TYPE_NONE )
 		{	//we are closed, do some last onClose work
@@ -1141,7 +1141,7 @@ void Con_RunConsole()
 	}
 	else if ( consoleState.isOpened && consoleState.currentAnimationFraction <= 1.0f)
 	{
-		consoleState.currentAnimationFraction += con_animationSpeed->value * cls.realFrametime * 0.001;
+		consoleState.currentAnimationFraction += con_animationSpeed->value * cls.realFrametime * 0.001f;
 
 		if ( consoleState.currentAnimationFraction > 1.0f  || con_animationType->integer == ANIMATION_TYPE_NONE  )
 		{
@@ -1154,7 +1154,7 @@ void Con_RunConsole()
 		const float scrollDifference = std::max( 0.5f, fabsf( consoleState.bottomDisplayedLine - consoleState.scrollLineIndex ) );
 		if( consoleState.bottomDisplayedLine < consoleState.scrollLineIndex )
 		{
-			consoleState.bottomDisplayedLine += con_animationSpeed->value * cls.realFrametime * 0.005 * scrollDifference;
+			consoleState.bottomDisplayedLine += con_animationSpeed->value * cls.realFrametime * 0.005f * scrollDifference;
 			if( consoleState.bottomDisplayedLine > consoleState.scrollLineIndex || con_animationType->integer == ANIMATION_TYPE_NONE )
 			{
 				consoleState.bottomDisplayedLine = consoleState.scrollLineIndex;
@@ -1162,7 +1162,7 @@ void Con_RunConsole()
 		}
 		else if ( consoleState.bottomDisplayedLine > consoleState.scrollLineIndex )
 		{
-			consoleState.bottomDisplayedLine -= con_animationSpeed->value * cls.realFrametime * 0.005 * scrollDifference;
+			consoleState.bottomDisplayedLine -= con_animationSpeed->value * cls.realFrametime * 0.005f * scrollDifference;
 			if( consoleState.bottomDisplayedLine < consoleState.scrollLineIndex || con_animationType->integer == ANIMATION_TYPE_NONE )
 			{
 				consoleState.bottomDisplayedLine = consoleState.scrollLineIndex;

src/engine/client/cl_input.cpp
@@ -237,11 +237,11 @@ void CL_AdjustAngles()
 
 	if ( kb[ KB_SPEED ].active )
 	{
-		speed = 0.001 * cls.frametime * cl_anglespeedkey->value;
+		speed = 0.001f * cls.frametime * cl_anglespeedkey->value;
 	}
 	else
 	{
-		speed = 0.001 * cls.frametime;
+		speed = 0.001f * cls.frametime;
 	}
 
 	if ( !kb[ KB_STRAFE ].active )
@@ -448,11 +448,11 @@ void CL_JoystickMove( usercmd_t *cmd )
 
 	if ( kb[ KB_SPEED ].active )
 	{
-		anglespeed = 0.001 * cls.frametime * cl_anglespeedkey->value;
+		anglespeed = 0.001f * cls.frametime * cl_anglespeedkey->value;
 	}
 	else
 	{
-		anglespeed = 0.001 * cls.frametime;
+		anglespeed = 0.001f * cls.frametime;
 	}
 
 #ifdef __MACOS__
@@ -504,11 +504,11 @@ void CL_GameControllerMove( usercmd_t *cmd )
 
 	if ( kb[ KB_SPEED ].active )
 	{
-		anglespeed = 0.001 * cls.frametime * cl_anglespeedkey->value;
+		anglespeed = 0.001f * cls.frametime * cl_anglespeedkey->value;
 	}
 	else
 	{
-		anglespeed = 0.001 * cls.frametime;
+		anglespeed = 0.001f * cls.frametime;
 	}
 
 	cl.viewangles[ PITCH ] += anglespeed * cl_pitchspeed->value * ( cl.joystickAxis[ Util::ordinal(joystickAxis_t::AXIS_PITCH) ] / 127.0f );
@@ -577,8 +577,8 @@ void CL_MouseMove( usercmd_t *cmd )
 			// cl_mouseAccelOffset is the rate for which the acceleration will have doubled the non accelerated amplification
 			// NOTE: decouple the config cvars for independent acceleration setup along X and Y?
 
-			rate[ 0 ] = fabs( mx ) / ( float ) frame_msec;
-			rate[ 1 ] = fabs( my ) / ( float ) frame_msec;
+			rate[ 0 ] = fabsf( mx ) / ( float ) frame_msec;
+			rate[ 1 ] = fabsf( my ) / ( float ) frame_msec;
 			power[ 0 ] = powf( rate[ 0 ] / cl_mouseAccelOffset->value, cl_mouseAccel->value );
 			power[ 1 ] = powf( rate[ 1 ] / cl_mouseAccelOffset->value, cl_mouseAccel->value );
 

commit 48967002e74ece82193ff5e81f3536acaa02ab49

Fix fog image
Fixes a regression introduced in 7e7cb4f43e4fabdf54b7b3830e01ee362665fe3f.

src/engine/renderer/tr_image.cpp
@@ -2224,7 +2224,7 @@ float R_FogFactor( float s, float t )
 		return 0;
 	}
 
-	if ( t < 31.0f / 320.0f )
+	if ( t < 31.0f / 32.0f )
 	{
 		s *= ( t - 1.0f / 32.0f ) / ( 30.0f / 32.0f );
 	}