DaemonEngine/Daemon

commit f7e67a10e21a82024ba590f3b0de346e652ba917

Cleanup nuke non-UBO and non-integer texture paths

src/engine/renderer/glsl_source/computeLight_fp.glsl
@@ -211,13 +211,13 @@ const int lightsPerLayer = 16;
 
 #define idxs_t uvec4
 
-uniform usampler3D u_LightTilesInt;
+uniform usampler3D u_LightTiles;
 
 const uint numLayers = MAX_REF_LIGHTS / 256;
 const vec3 tileScale = vec3( r_tileStep, 1.0 / numLayers );
 
-idxs_t fetchIdxs( in vec3 coords, in usampler3D u_LightTilesInt ) {
-	return texture3D( u_LightTilesInt, coords );
+idxs_t fetchIdxs( in vec3 coords, in usampler3D u_LightTiles ) {
+	return texture3D( u_LightTiles, coords );
 }
 
 // 8 bits per light ID
@@ -227,11 +227,11 @@ uint nextIdx( in uint count, in idxs_t idxs ) {
 
 #if defined(USE_REFLECTIVE_SPECULAR)
 void computeDynamicLights( vec3 P, vec3 normal, vec3 viewDir, vec4 diffuse, vec4 material,
-	inout vec4 color, in usampler3D u_LightTilesInt,
+	inout vec4 color, in usampler3D u_LightTiles,
 	in samplerCube u_EnvironmentMap0, in samplerCube u_EnvironmentMap1 )
 #else // !USE_REFLECTIVE_SPECULAR
 void computeDynamicLights( vec3 P, vec3 normal, vec3 viewDir, vec4 diffuse, vec4 material,
-	inout vec4 color, in usampler3D u_LightTilesInt )
+	inout vec4 color, in usampler3D u_LightTiles )
 #endif // !USE_REFLECTIVE_SPECULAR
 {
 	vec2 tile = floor( gl_FragCoord.xy * ( 1.0 / float( TILE_SIZE ) ) ) + 0.5;
@@ -241,7 +241,7 @@ void computeDynamicLights( vec3 P, vec3 normal, vec3 viewDir, vec4 diffuse, vec4
 
 	uint lightCount = 0;
 	for( uint layer = 0; layer < numLayers; layer++ ) {
-		idxs_t idxs = fetchIdxs( tileScale * vec3( tile, float( layer ) + 0.5 ), u_LightTilesInt );
+		idxs_t idxs = fetchIdxs( tileScale * vec3( tile, float( layer ) + 0.5 ), u_LightTiles );
 
 		uint lightOffset = layer * globalLightsPerLayer;
 		uint layerLightCount = lightOffset < u_numLights ? min( min( u_numLights - lightOffset, globalLightsPerLayer ), lightsPerLayer ) : 0;

src/engine/renderer/Material.cpp
@@ -307,7 +307,7 @@ void UpdateSurfaceDataLightMapping( uint32_t* materials, Material& material, dra
 
 		// bind u_LightTiles
 		if ( r_realtimeLightingRenderer.Get() == Util::ordinal( realtimeLightingRenderer_t::TILED ) ) {
-			gl_lightMappingShaderMaterial->SetUniform_LightTilesIntBindless(
+			gl_lightMappingShaderMaterial->SetUniform_LightTilesBindless(
 				GL_BindToTMU( BIND_LIGHTTILES, tr.lighttileRenderImage )
 			);
 		}

src/engine/renderer/gl_shader.cpp
@@ -2289,8 +2289,6 @@ GLShader_lightMapping::GLShader_lightMapping( GLShaderManager *manager ) :
 	u_LightGrid1( this ),
 	u_LightGrid2( this ),
 	u_LightTiles( this ),
-	u_LightTilesInt( this ),
-	u_LightsTexture( this ),
 	u_TextureMatrix( this ),
 	u_SpecularExponent( this ),
 	u_ColorModulate( this ),
@@ -2338,7 +2336,6 @@ void GLShader_lightMapping::SetShaderProgramUniforms( shaderProgram_t *shaderPro
 	glUniform1i( glGetUniformLocation( shaderProgram->program, "u_EnvironmentMap0" ), BIND_ENVIRONMENTMAP0 );
 	glUniform1i( glGetUniformLocation( shaderProgram->program, "u_EnvironmentMap1" ), BIND_ENVIRONMENTMAP1 );
 	glUniform1i( glGetUniformLocation( shaderProgram->program, "u_LightTiles" ), BIND_LIGHTTILES );
-	glUniform1i( glGetUniformLocation( shaderProgram->program, "u_LightTilesInt" ), BIND_LIGHTTILES );
 	if( !glConfig2.uniformBufferObjectAvailable ) {
 		glUniform1i( glGetUniformLocation( shaderProgram->program, "u_Lights" ), BIND_LIGHTS );
 	}
@@ -2358,7 +2355,7 @@ GLShader_lightMappingMaterial::GLShader_lightMappingMaterial( GLShaderManager* m
 	u_EnvironmentMap1( this ),
 	u_LightGrid1( this ),
 	u_LightGrid2( this ),
-	u_LightTilesInt( this ),
+	u_LightTiles( this ),
 	u_TextureMatrix( this ),
 	u_SpecularExponent( this ),
 	u_ColorModulate( this ),
@@ -3021,7 +3018,7 @@ GLShader_liquidMaterial::GLShader_liquidMaterial( GLShaderManager* manager ) :
 	u_NormalScale( this ),
 	u_FogDensity( this ),
 	u_FogColor( this ),
-	u_LightTilesInt( this ),
+	u_LightTiles( this ),
 	u_SpecularExponent( this ),
 	u_LightGridOrigin( this ),
 	u_LightGridScale( this ),
@@ -3098,7 +3095,6 @@ GLShader_lighttile::GLShader_lighttile( GLShaderManager *manager ) :
 	GLShader( "lighttile", ATTR_POSITION | ATTR_TEXCOORD, manager ),
 	u_DepthMap( this ),
 	u_Lights( this ),
-	u_LightsTexture( this ),
 	u_numLights( this ),
 	u_lightLayer( this ),
 	u_ModelMatrix( this ),

src/engine/renderer/gl_shader.h
@@ -2425,22 +2425,6 @@ class u_CloudMap :
 	}
 };
 
-class u_LightsTexture :
-	GLUniformSampler2D {
-	public:
-	u_LightsTexture( GLShader* shader ) :
-		GLUniformSampler2D( shader, "u_LightsTexture" ) {
-	}
-
-	void SetUniform_LightsTextureBindless( GLuint64 bindlessHandle ) {
-		this->SetValueBindless( bindlessHandle );
-	}
-
-	GLint GetUniformLocation_LightsTexture() {
-		return this->GetLocation();
-	}
-};
-
 class u_LightTiles :
 	GLUniformSampler3D {
 	public:
@@ -2453,22 +2437,6 @@ class u_LightTiles :
 	}
 
 	GLint GetUniformLocation_LightTiles() {
-		return this->GetLocation();
-	}
-};
-
-class u_LightTilesInt :
-	GLUniformUSampler3D {
-	public:
-	u_LightTilesInt( GLShader* shader ) :
-		GLUniformUSampler3D( shader, "u_LightTilesInt" ) {
-	}
-
-	void SetUniform_LightTilesIntBindless( GLuint64 bindlessHandle ) {
-		this->SetValueBindless( bindlessHandle );
-	}
-
-	GLint GetUniformLocation_LightTilesInt() {
 		return this->GetLocation();
 	}
 };
@@ -4023,8 +3991,6 @@ class GLShader_lightMapping :
 	public u_LightGrid1,
 	public u_LightGrid2,
 	public u_LightTiles,
-	public u_LightTilesInt,
-	public u_LightsTexture,
 	public u_TextureMatrix,
 	public u_SpecularExponent,
 	public u_ColorModulate,
@@ -4074,7 +4040,7 @@ class GLShader_lightMappingMaterial :
 	public u_EnvironmentMap1,
 	public u_LightGrid1,
 	public u_LightGrid2,
-	public u_LightTilesInt,
+	public u_LightTiles,
 	public u_TextureMatrix,
 	public u_SpecularExponent,
 	public u_ColorModulate,
@@ -4629,7 +4595,7 @@ class GLShader_liquidMaterial :
 	public u_NormalScale,
 	public u_FogDensity,
 	public u_FogColor,
-	public u_LightTilesInt,
+	public u_LightTiles,
 	public u_SpecularExponent,
 	public u_LightGridOrigin,
 	public u_LightGridScale,
@@ -4689,7 +4655,6 @@ class GLShader_lighttile :
 	public GLShader,
 	public u_DepthMap,
 	public u_Lights,
-	public u_LightsTexture,
 	public u_numLights,
 	public u_lightLayer,
 	public u_ModelMatrix,

src/engine/renderer/glsl_source/lightMapping_fp.glsl
@@ -193,10 +193,10 @@ void main()
 	// Blend dynamic lights.
 	#if defined(r_realtimeLighting) && r_realtimeLightingRenderer == 1
 		#if defined(USE_REFLECTIVE_SPECULAR)
-			computeDynamicLights(var_Position, normal, viewDir, diffuse, material, color, u_LightTilesInt,
+			computeDynamicLights(var_Position, normal, viewDir, diffuse, material, color, u_LightTiles,
 								 u_EnvironmentMap0, u_EnvironmentMap1);
 		#else // !USE_REFLECTIVE_SPECULAR
-			computeDynamicLights(var_Position, normal, viewDir, diffuse, material, color, u_LightTilesInt);
+			computeDynamicLights(var_Position, normal, viewDir, diffuse, material, color, u_LightTiles);
 		#endif // !USE_REFLECTIVE_SPECULAR
 	#endif
 

src/engine/renderer/glsl_source/material_fp.glsl
@@ -57,7 +57,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 samplerCube u_EnvironmentMap0 = samplerCube( u_EnvironmentMap0_initial );
 samplerCube u_EnvironmentMap1 = samplerCube( u_EnvironmentMap1_initial );
 #endif // !USE_REFLECTIVE_SPECULAR
-usampler3D u_LightTilesInt = usampler3D( u_LightTilesInt_initial );
+usampler3D u_LightTiles = usampler3D( u_LightTiles_initial );
 #endif // !COMPUTELIGHT_GLSL
 
 #if defined(FOGQUAKE3_GLSL)

src/engine/renderer/tr_backend.cpp
@@ -2905,13 +2905,7 @@ void RB_RenderPostDepthLightTile()
 	gl_lighttileShader->SetUniform_numLights( backEnd.refdef.numLights );
 	gl_lighttileShader->SetUniform_zFar( projToViewParams );
 
-	if( glConfig2.uniformBufferObjectAvailable ) {
-		gl_lighttileShader->SetUniformBlock_Lights( tr.dlightUBO );
-	} else {
-		gl_lighttileShader->SetUniform_LightsTextureBindless(
-			GL_BindToTMU( 1, tr.dlightImage ) 
-		);
-	}
+	gl_lighttileShader->SetUniformBlock_Lights( tr.dlightUBO );
 
 	gl_lighttileShader->SetUniform_DepthMapBindless(
 		GL_BindToTMU( 1, tr.depthtile2RenderImage ) 
@@ -5370,12 +5364,6 @@ const RenderCommand *SetupLightsCommand::ExecuteSelf( ) const
 		}
 
 		glUnmapBuffer( bufferTarget );
-		if( !glConfig2.uniformBufferObjectAvailable ) {
-			gl_lighttileShader->SetUniform_LightsTextureBindless(
-				GL_BindToTMU( 1, tr.dlightImage ) 
-			);
-			glTexSubImage2D( GL_TEXTURE_2D, 0, 0, 0, tr.dlightImage->width, tr.dlightImage->height, GL_RGBA, GL_FLOAT, nullptr );
-		}
 		glBindBuffer( bufferTarget, 0 );
 	}
 

src/engine/renderer/tr_image.cpp
@@ -2482,21 +2482,6 @@ static void R_CreateDepthRenderImage()
 		return;
 	}
 
-	if( !glConfig2.uniformBufferObjectAvailable )
-	{
-		int w = 64;
-		int h = 3 * MAX_REF_LIGHTS / w;
-
-		imageParams_t imageParams = {};
-		imageParams.filterType = filterType_t::FT_NEAREST;
-		imageParams.wrapType = wrapTypeEnum_t::WT_CLAMP;
-
-		imageParams.bits = IF_NOPICMIP;
-		imageParams.bits |= r_highPrecisionRendering.Get() ? IF_RGBA32F : IF_RGBA16F;
-
-		tr.dlightImage = R_CreateImage("_dlightImage", nullptr, w, h, 4, imageParams );
-	}
-
 	if ( r_realtimeLightingRenderer.Get() != Util::ordinal( realtimeLightingRenderer_t::TILED ) )
 	{
 		/* Do not create lightTile images when the tiled renderer is not used.
@@ -2536,12 +2521,7 @@ static void R_CreateDepthRenderImage()
 
 		tr.depthtile2RenderImage = R_CreateImage( "_depthtile2Render", nullptr, w, h, 1, imageParams );
 
-		imageParams.bits = IF_NOPICMIP;
-
-		if ( glConfig2.textureIntegerAvailable && r_highPrecisionRendering.Get() )
-		{
-			imageParams.bits |= IF_RGBA32UI;
-		}
+		imageParams.bits = IF_NOPICMIP | IF_RGBA32UI;
 
 		tr.lighttileRenderImage = R_Create3DImage( "_lighttileRender", nullptr, w, h, 4, imageParams );
 	}

commit fbe4a3b7765d0b8d5805f63d108b56d24469bc19

Fix light ID computation
Fix a bug introduced in #1333.

src/engine/renderer/glsl_source/computeLight_fp.glsl
@@ -239,8 +239,8 @@ void computeDynamicLights( vec3 P, vec3 normal, vec3 viewDir, vec4 diffuse, vec4
 	// NOT the amount of lights that can actually be put into each layer of the lighttile texture
 	uint globalLightsPerLayer = ( u_numLights + numLayers - 1 ) / numLayers;
 
-	uint lightCount = 0;
 	for( uint layer = 0; layer < numLayers; layer++ ) {
+		uint lightCount = 0;
 		idxs_t idxs = fetchIdxs( tileScale * vec3( tile, float( layer ) + 0.5 ), u_LightTiles );
 
 		uint lightOffset = layer * globalLightsPerLayer;
@@ -254,7 +254,7 @@ void computeDynamicLights( vec3 P, vec3 normal, vec3 viewDir, vec4 diffuse, vec4
 
 			/* Light IDs are stored relative to the layer
 			Subtract 1 because 0 means there's no light */
-			idx += ( layer * numLayers ) + layer - 1;
+			idx = ( idx - 1 ) * numLayers + layer;
 	  
 			#if defined(USE_REFLECTIVE_SPECULAR)
 				computeDynamicLight( idx, P, normal, viewDir, diffuse, material, color, u_EnvironmentMap0, u_EnvironmentMap1 );

src/engine/renderer/glsl_source/lighttile_fp.glsl
@@ -123,7 +123,7 @@ void main() {
 	only process 1 / 4 of different lights for each layer, extra lights going into the last layer. This can fail to add some lights
 	if 1 / 4 of all lights is more than the amount of lights that each layer can hold (16). To fix this, we'd need to either do this on CPU
 	or use compute shaders with atomics so we can have a variable amount of lights for each tile. */
-	for( uint i = u_lightLayer; i < u_numLights; i += 4 ) {
+	for( uint i = u_lightLayer; i < u_numLights; i += numLayers ) {
 		Light l = GetLight( i );
 		vec3 center = ( u_ModelMatrix * vec4( l.center, 1.0 ) ).xyz;
 		float radius = max( 2.0 * l.radius, 2.0 * 32.0 ); // Avoid artifacts with weak light sources
@@ -139,7 +139,7 @@ void main() {
 		if( radius > 0.0 ) {
 			/* Light IDs are stored relative to the layer
 			Add 1 because 0 means there's no light */
-			pushIdxs( i + 1, lightCount, idxs );
+			pushIdxs( ( i / numLayers ) + 1, lightCount, idxs );
 			lightCount++;
 
 			if( lightCount == lightsPerLayer ) {