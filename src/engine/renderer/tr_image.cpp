12559766a3dc1da267f8184907f6fc6180977dcb
VReaperV <reaper9977@gmail.com>
2024-10-16 22:26:35 +0300

    Fix R_CreateImage() crash with r_logFile
    Fixes a crash resulting from `va()` overflow.

--- src/engine/renderer/tr_image.cpp
+++ src/engine/renderer/tr_image.cpp
@@ -3052,8 +3052,9 @@ qhandle_t RE_GenerateTexture( const byte *pic, int width, int height )
 		return 0;
 	}
 
-	const char *name = va( "rocket%d", numTextures++ );
 	R_SyncRenderThread();
+
+	const char *name = va( "rocket%d", numTextures++ );
 
 	imageParams_t imageParams = {};
 	imageParams.bits = IF_NOPICMIP;
