ea1b9f7881dadee29bc6de39def461105f1fcbe4
slipher <slipher@protonmail.com>
2024-10-18 05:43:32 -0500

    Fix some warnings with assertions enabled

--- src/engine/renderer/tr_local.h
+++ src/engine/renderer/tr_local.h
@@ -2512,9 +2512,9 @@ enum class cubeProbesAutoBuildMode {
 				z = Math::Clamp( z, 0u, depth - 1 );
 			}
 
-			ASSERT_GE( x, 0 );
-			ASSERT_GE( y, 0 );
-			ASSERT_GE( z, 0 );
+			ASSERT_GE( x, 0u );
+			ASSERT_GE( y, 0u );
+			ASSERT_GE( z, 0u );
 
 			ASSERT_LT( x, width );
 			ASSERT_LT( y, height );
@@ -2538,7 +2538,7 @@ enum class cubeProbesAutoBuildMode {
 				index = Math::Clamp( index, 0u, size - 1 );
 			}
 
-			ASSERT_GE( index, 0 );
+			ASSERT_GE( index, 0U );
 
 			ASSERT_LT( index, size );
 
