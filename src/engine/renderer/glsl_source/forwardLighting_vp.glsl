3e4bdd14a41b4a1011f646e738016bd275a78332
VReaperV <reaper9977@gmail.com>
2024-12-25 15:05:44 +0300

    Change u_Color to uint
    `u_Color` was a vec4 that was being set from a `Color` object, which only has 8-bit precision per colour, so using up 4 times more memory than that to transfer it was quite atrocious. Instead, pack it into a uint32_t, then use `unpackUnorm4x8()` to unpack in the shader.
    
    Also adds a macro for unpacking for hardware that doesn't support `GL_ARB_gpu_shader5`.

--- src/engine/renderer/glsl_source/forwardLighting_vp.glsl
+++ src/engine/renderer/glsl_source/forwardLighting_vp.glsl
@@ -32,7 +32,7 @@ uniform mat4		u_ModelMatrix;
 uniform mat4		u_ModelViewProjectionMatrix;
 
 uniform vec4		u_ColorModulate;
-uniform vec4		u_Color;
+uniform uint		u_Color;
 
 uniform float		u_Time;
 
@@ -63,7 +63,7 @@ void	main()
 	VertexFetch( position, LB, color, texCoord, lmCoord);
 
 	// assign color
-        color = color * u_ColorModulate + u_Color;
+        color = color * u_ColorModulate + unpackUnorm4x8( u_Color );
 
 	DeformVertex( position,
 		      LB.normal,
