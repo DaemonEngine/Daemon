313590288242b88d9afc113a2c0a69ff123262c3
VReaperV <reaper9977@gmail.com>
2024-12-26 01:44:22 +0300

    Add r_materialSystemSkip
    This makes debugging issues that are present on material system but no core renderer much faster, since it can be changed without any sort of restart.

--- src/engine/renderer/Material.cpp
+++ src/engine/renderer/Material.cpp
@@ -2052,6 +2052,10 @@ void MaterialSystem::RenderMaterials( const shaderSort_t fromSort, const shaderS
 		return;
 	}
 
+	if ( r_materialSystemSkip.Get() ) {
+		return;
+	}
+
 	if ( frameStart ) {
 		renderedMaterials.clear();
 		UpdateDynamicSurfaces();
