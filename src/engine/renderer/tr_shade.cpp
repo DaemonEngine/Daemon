a392fb0b762203a2c70a7d5e639442cdb791f997
VReaperV <reaper9977@gmail.com>
2025-01-02 14:25:37 +0300

    Fix drawSurf-specific data with material system

--- src/engine/renderer/tr_shade.cpp
+++ src/engine/renderer/tr_shade.cpp
@@ -2356,7 +2356,7 @@ void Render_fog( shaderStage_t* pStage )
 	gl_fogQuake3Shader->SetUniform_FogEyeT( eyeT );
 
 	// u_Color
-	gl_fogQuake3Shader->SetUniform_Color( fog->color );
+	gl_fogQuake3Shader->SetUniform_ColorGlobal( fog->color );
 
 	gl_fogQuake3Shader->SetUniform_ModelMatrix( backEnd.orientation.transformMatrix );
 	gl_fogQuake3Shader->SetUniform_ModelViewProjectionMatrix( glState.modelViewProjectionMatrix[ glState.stackIndex ] );
@@ -2743,8 +2743,8 @@ void Tess_StageIteratorColor()
 
 		if ( materialSystem.generatingWorldCommandBuffer && pStage->useMaterialSystem ) {
 			tess.currentSSBOOffset = pStage->materialOffset;
-			tess.materialID = pStage->materialID;
-			tess.materialPackID = pStage->materialPackID;
+			tess.materialID = tess.currentDrawSurf->materialIDs[stage];
+			tess.materialPackID = tess.currentDrawSurf->materialPackIDs[stage];
 		}
 
 		pStage->colorRenderer( pStage );
