7406196da3c1a4439c58dd8ccb53ef78a153b390
Johan Mattsson <39247600+j-mattsson@users.noreply.github.com>
2025-01-02 21:39:54 +0100

    Fix potential index out of bounds in Material.cpp

--- src/engine/renderer/Material.cpp
+++ src/engine/renderer/Material.cpp
@@ -1950,7 +1950,7 @@ void MaterialSystem::AddDrawCommand( const uint32_t materialID, const uint32_t m
 }
 
 void MaterialSystem::AddTexture( Texture* texture ) {
-	if ( cmd.textureCount > MAX_DRAWCOMMAND_TEXTURES ) {
+	if ( cmd.textureCount >= MAX_DRAWCOMMAND_TEXTURES ) {
 		Sys::Drop( "Exceeded max DrawCommand textures" );
 	}
 	cmd.textures[cmd.textureCount] = texture;
