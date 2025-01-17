396d7ee5d71470182177667ca16b8223b25f81e6
VReaperV <reaper9977@gmail.com>
2025-01-18 02:09:47 +0300

    Clean-up material system buffer bindings

--- src/engine/renderer/Material.h
+++ src/engine/renderer/Material.h
@@ -277,6 +277,20 @@ struct SurfaceCommandBatch {
 	uint32_t materialIDs[2] { 0, 0 };
 };
 
+enum class BufferBind {
+	MATERIALS = 1, // LightTile UBO uses binding point 0, so avoid it here
+	TEX_DATA = 6,
+	LIGHTMAP_DATA = 2,
+	SURFACE_DESCRIPTORS = 0,
+	SURFACE_COMMANDS = 1,
+	CULLED_COMMANDS = 2,
+	SURFACE_BATCHES = 3,
+	COMMAND_COUNTERS_ATOMIC = 0,
+	COMMAND_COUNTERS_STORAGE = 4, // Avoid needlessly rebinding buffers
+	PORTAL_SURFACES = 5,
+	DEBUG = 10
+};
+
 class MaterialSystem {
 	public:
 	bool generatedWorldCommandBuffer = false;
