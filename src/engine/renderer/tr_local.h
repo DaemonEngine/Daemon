d33fe99c0a4eb033ac076640f5a3dba759312bcd
VReaperV <reaper9977@gmail.com>
2025-01-27 18:07:40 +0300

    NUKE useless GL_ARB_buffer_storage and GL_ARB_sync macro checks
    These are already inherently a part of `glConfig2.bufferStorageAvailable` and `glConfig2.syncAvailable`, the macros do nothing.

--- src/engine/renderer/tr_local.h
+++ src/engine/renderer/tr_local.h
@@ -3417,7 +3417,6 @@ inline bool checkGLErrors()
 		vec4_t texCoords;
 	};
 
-#ifdef GL_ARB_sync
 	struct glRingbuffer_t {
 		// the BO is logically split into DYN_BUFFER_SEGMENTS
 		// segments, the active segment is the one the CPU may write
@@ -3431,7 +3430,6 @@ inline bool checkGLErrors()
 		// sync is always undefined
 		GLsync         syncs[ DYN_BUFFER_SEGMENTS ];
 	};
-#endif
 
 	struct shaderCommands_t
 	{
@@ -3501,10 +3499,8 @@ inline bool checkGLErrors()
 		VBO_t       *vbo;
 		IBO_t       *ibo;
 
-#ifdef GL_ARB_sync
 		glRingbuffer_t  vertexRB;
 		glRingbuffer_t  indexRB;
-#endif
 	};
 
 	alignas(16) extern shaderCommands_t tess;
