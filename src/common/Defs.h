6b82e5cf88ad22c4b174ed84e0794644b15babfc
Thomas Debesse <dev@illwieckz.net>
2025-04-14 20:00:00 +0000

    bump version number to 0.55.3

--- src/common/Defs.h
+++ src/common/Defs.h
@@ -37,7 +37,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 /** No case, No spaces */
 #define PRODUCT_NAME_LOWER  "unvanquished"
 
-#define PRODUCT_VERSION     "0.55.2"
+#define PRODUCT_VERSION     "0.55.3"
 
 /** Default base package */
 #define DEFAULT_BASE_PAK    PRODUCT_NAME_LOWER
