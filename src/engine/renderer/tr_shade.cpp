9b7b39002b0ff4c8b2272ad4170aaf0827e376a5
VReaperV <reaper9977@gmail.com>
2025-06-19 00:12:31 +0300

    Defer shader building
    This will be required for PushBuffer.

--- src/engine/renderer/tr_shade.cpp
+++ src/engine/renderer/tr_shade.cpp
@@ -190,10 +190,10 @@ void GLSL_InitWorldShaders() {
 
 	// Material system shaders that are always loaded if material system is available
 	if ( glConfig2.usingMaterialSystem ) {
-		gl_shaderManager.LoadShader( gl_cullShader );
-
 		gl_cullShader->MarkProgramForBuilding( 0 );
 	}
+
+	gl_shaderManager.InitWorldShaders();
 }
 
 static void GLSL_InitGPUShadersOrError()
@@ -224,6 +224,9 @@ static void GLSL_InitGPUShadersOrError()
 		gl_shaderManager.LoadShader( gl_lightMappingShaderMaterial );
 
 		gl_shaderManager.LoadShader( gl_clearSurfacesShader );
+		/* Load gl_cullShader so we can post-process it correctly for push buffer,
+		it will only actually be built in GLSL_InitWorldShaders() */
+		gl_shaderManager.LoadShader( gl_cullShader );
 		gl_shaderManager.LoadShader( gl_processSurfacesShader );
 		gl_shaderManager.LoadShader( gl_depthReductionShader );
 
@@ -320,7 +323,6 @@ static void GLSL_InitGPUShadersOrError()
 
 		gl_contrastShader->MarkProgramForBuilding( 0 );
 	}
-
 	
 	// portal process effect
 	gl_shaderManager.LoadShader( gl_portalShader );
@@ -370,6 +372,8 @@ static void GLSL_InitGPUShadersOrError()
 
 		gl_fxaaShader->MarkProgramForBuilding( 0 );
 	}
+
+	gl_shaderManager.InitShaders();
 
 	if ( r_lazyShaders.Get() == 0 )
 	{
