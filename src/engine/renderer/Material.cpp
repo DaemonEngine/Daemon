3f4e4cd4716eeca9fd9e3e20380fbe7ffc1fe76d
VReaperV <reaper9977@gmail.com>
2025-06-23 12:29:17 +0300

    NUKE useless buffer unmapping and R_SyncRenderThread() in material system
    Deleting the buffer automatically unmaps it.

--- src/engine/renderer/Material.cpp
+++ src/engine/renderer/Material.cpp
@@ -1729,17 +1729,7 @@ void MaterialSystem::Free() {
 	texData.clear();
 	dynamicTexData.clear();
 
-	R_SyncRenderThread();
-
-	surfaceCommandsSSBO.UnmapBuffer();
-	culledCommandsBuffer.UnmapBuffer();
-	atomicCommandCountersBuffer.UnmapBuffer();
-	texDataBuffer.UnmapBuffer();
-	lightMapDataUBO.UnmapBuffer();
-
 	if ( totalPortals > 0 ) {
-		portalSurfacesSSBO.UnmapBuffer();
-
 		for ( PortalView& portalView : portalStack ) {
 			memset( portalView.views, 0, MAX_VIEWS * sizeof( uint32_t ) );
 			portalView.count = 0;
