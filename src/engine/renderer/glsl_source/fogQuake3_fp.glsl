f4102cbc4459c09dec560eb29889ddb44b24efcb
VReaperV <reaper9977@gmail.com>
2025-06-24 20:47:00 +0300

    Fix global fog texture with PushBuffer

--- src/engine/renderer/glsl_source/fogQuake3_fp.glsl
+++ src/engine/renderer/glsl_source/fogQuake3_fp.glsl
@@ -22,7 +22,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
 /* fogQuake3_fp.glsl */
 
-#define FOGQUAKE3_GLSL
+#define FOGMAP_GLSL
 
 uniform sampler2D u_FogMap;
 
