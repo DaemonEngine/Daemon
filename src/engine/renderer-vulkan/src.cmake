d06bb5388c9f648f359e4a73a0886006ea489ee5
VReaperV <reaper9977@gmail.com>
2025-06-30 23:33:14 +0300

    Fix directory name

--- src/engine/renderer-vulkan/src.cmake
+++ src/engine/renderer-vulkan/src.cmake
@@ -1,6 +1,6 @@
 set(RENDERERLIST
     ${ENGINE_DIR}/renderer-vulkan/RefAPI.cpp
     ${ENGINE_DIR}/renderer-vulkan/RefAPI.h
-    ${ENGINE_DIR}/renderer-vulkan/shared/Timer.cpp
-    ${ENGINE_DIR}/renderer-vulkan/shared/Timer.h
+    ${ENGINE_DIR}/renderer-vulkan/Shared/Timer.cpp
+    ${ENGINE_DIR}/renderer-vulkan/Shared/Timer.h
 )
