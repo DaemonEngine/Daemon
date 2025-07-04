68878d1dba13c6b7314b045f856f567b1b69e8b9
VReaperV <reaper9977@gmail.com>
2025-07-05 00:53:07 +0300

    Add LogExtend and MiscCVarStore

--- src/engine/renderer-vulkan/src.cmake
+++ src/engine/renderer-vulkan/src.cmake
@@ -2,8 +2,14 @@ set(MATHLIST
     ${ENGINE_DIR}/renderer-vulkan/Math/Bit.h
 )
 
+set(UTILSLIST
+    ${ENGINE_DIR}/renderer-vulkan/SrcDebug/LogExtend.h
+)
+
 set(RENDERERLIST
     ${MATHLIST}
+    ${UTILSLIST}
+    ${ENGINE_DIR}/renderer-vulkan/MiscCVarStore.cpp
     ${ENGINE_DIR}/renderer-vulkan/RefAPI.cpp
     ${ENGINE_DIR}/renderer-vulkan/RefAPI.h
     ${ENGINE_DIR}/renderer-vulkan/Shared/Timer.cpp
