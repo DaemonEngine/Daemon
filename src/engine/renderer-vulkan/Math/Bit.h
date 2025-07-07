fab278f99990ac15ab01d02f6d34daa5c2b2ae32
VReaperV <reaper9977@gmail.com>
2025-07-07 12:19:36 +0300

    Make Bit stuff inline

--- src/engine/renderer-vulkan/Math/Bit.h
+++ src/engine/renderer-vulkan/Math/Bit.h
@@ -40,75 +40,75 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #if defined(DAEMON_USE_COMPILER_INTRINSICS) && defined(__GNUC__)
 
-uint32_t FindLSB( const uint32_t value ) {
-	return value ? __builtin_ctzl( value ) : 32;
-}
+	inline uint32_t FindLSB( const uint32_t value ) {
+		return value ? __builtin_ctzl( value ) : 32;
+	}
 
-uint32_t FindLSB( const uint64_t value ) {
-	return value ? __builtin_ctzll( value ) : 64;
-}
+	inline uint32_t FindLSB( const uint64_t value ) {
+		return value ? __builtin_ctzll( value ) : 64;
+	}
 
-uint32_t FindMSB( const uint32_t value ) {
-	return value ? __builtin_clzl( value ) : 32;
-}
+	inline uint32_t FindMSB( const uint32_t value ) {
+		return value ? __builtin_clzl( value ) : 32;
+	}
 
-uint32_t FindMSB( const uint64_t value ) {
-	return value ? __builtin_clzll( value ) : 64;
-}
+	inline uint32_t FindMSB( const uint64_t value ) {
+		return value ? __builtin_clzll( value ) : 64;
+	}
 
-uint16_t CountLeadingZeroes( const uint16_t value ) {
-	return FindLSB( value );
-}
+	inline uint16_t CountLeadingZeroes( const uint16_t value ) {
+		return FindLSB( value );
+	}
 
-uint32_t CountLeadingZeroes( const uint32_t value ) {
-	return FindLSB( value );
-}
+	inline uint32_t CountLeadingZeroes( const uint32_t value ) {
+		return FindLSB( value );
+	}
 
-uint64_t CountLeadingZeroes( const uint64_t value ) {
-	return FindLSB( value );
-}
+	inline uint64_t CountLeadingZeroes( const uint64_t value ) {
+		return FindLSB( value );
+	}
 
 #elif defined(DAEMON_USE_COMPILER_INTRINSICS) && defined(_MSC_VER)
 
-	uint32_t FindLSB( const uint32_t value ) {
+	inline uint32_t FindLSB( const uint32_t value ) {
 		unsigned long index;
 		const bool nonZero = _BitScanForward( &index, value );
 		return nonZero ? index : 32;
 	}
 
-	uint32_t FindLSB( const uint64_t value ) {
+	inline uint32_t FindLSB( const uint64_t value ) {
 		unsigned long index;
 		const bool nonZero = _BitScanForward64( &index, value );
 		return nonZero ? index : 64;
 	}
 
-	uint32_t FindMSB( const uint32_t value ) {
+	inline uint32_t FindMSB( const uint32_t value ) {
 		unsigned long index;
 		const bool nonZero = _BitScanReverse( &index, value );
 		return nonZero ? index : 32;
 	}
 
-	uint32_t FindMSB( const uint64_t value ) {
+	inline uint32_t FindMSB( const uint64_t value ) {
 		unsigned long index;
 		const bool nonZero = _BitScanReverse64( &index, value );
 		return nonZero ? index : 64;
 	}
 
-	uint16_t CountLeadingZeroes( const uint16_t value ) {
+	inline uint16_t CountLeadingZeroes( const uint16_t value ) {
 		return __lzcnt16( value );
 	}
 
-	uint32_t CountLeadingZeroes( const uint32_t value ) {
+	inline uint32_t CountLeadingZeroes( const uint32_t value ) {
 		return __lzcnt( value );
 	}
 
-	uint64_t CountLeadingZeroes( const uint64_t value ) {
+	inline uint64_t CountLeadingZeroes( const uint64_t value ) {
 		return __lzcnt64( value );
 	}
 
 #else
 
-	uint32_t FindLSB( const uint32_t value ) {
+	inline uint32_t FindLSB( const uint32_t value ) {
 		for ( int i = 0; i < 32; i++ ) {
 			if ( value & ( 1u << i ) ) {
 				return i;
@@ -118,7 +118,7 @@ uint64_t CountLeadingZeroes( const uint64_t value ) {
 		return 32;
 	}
 
-	uint32_t FindLSB( const uint64_t value ) {
+	inline uint32_t FindLSB( const uint64_t value ) {
 		for ( int i = 0; i < 64; i++ ) {
 			if ( value & ( 1ull << i ) ) {
 				return i;
@@ -128,7 +128,7 @@ uint64_t CountLeadingZeroes( const uint64_t value ) {
 		return 64;
 	}
 
-	uint32_t FindMSB( const uint32_t value ) {
+	inline uint32_t FindMSB( const uint32_t value ) {
 		for ( int i = 31; i >= 0; i-- ) {
 			if ( value & ( 1u << i ) ) {
 				return i;
@@ -138,7 +138,7 @@ uint64_t CountLeadingZeroes( const uint64_t value ) {
 		return 32;
 	}
 
-	uint32_t FindMSB( const uint64_t value ) {
+	inline uint32_t FindMSB( const uint64_t value ) {
 		for ( int i = 63; i >= 0; i-- ) {
 			if ( value & ( 1ull << i ) ) {
 				return i;
@@ -148,125 +148,125 @@ uint64_t CountLeadingZeroes( const uint64_t value ) {
 		return 63;
 	}
 
-	uint16_t CountLeadingZeroes( const uint16_t value ) {
+	inline uint16_t CountLeadingZeroes( const uint16_t value ) {
 		return value ? FindLSB( value ) - 1 : 0;
 	}
 
-	uint32_t CountLeadingZeroes( const uint32_t value ) {
+	inline uint32_t CountLeadingZeroes( const uint32_t value ) {
 		return value ? FindLSB( value ) - 1 : 0;
 	}
 
-	uint64_t CountLeadingZeroes( const uint64_t value ) {
+	inline uint64_t CountLeadingZeroes( const uint64_t value ) {
 		return value ? FindLSB( value ) - 1 : 0;
 	}
 
 #endif
 
-[[nodiscard]] uint8_t SetBit( const uint8_t value, const uint32_t bit ) {
+[[nodiscard]] inline uint8_t SetBit( const uint8_t value, const uint32_t bit ) {
 	return value | ( 1u << bit );
 }
 
-[[nodiscard]] uint16_t SetBit( const uint16_t value, const uint32_t bit ) {
+[[nodiscard]] inline uint16_t SetBit( const uint16_t value, const uint32_t bit ) {
 	return value | ( 1u << bit );
 }
 
-[[nodiscard]] uint32_t SetBit( const uint32_t value, const uint32_t bit ) {
+[[nodiscard]] inline uint32_t SetBit( const uint32_t value, const uint32_t bit ) {
 	return value | ( 1u << bit );
 }
 
-[[nodiscard]] uint64_t SetBit( const uint64_t value, const uint32_t bit ) {
+[[nodiscard]] inline uint64_t SetBit( const uint64_t value, const uint32_t bit ) {
 	return value | ( 1ull << bit );
 }
 
-void SetBit( uint8_t* value, const uint32_t bit ) {
+inline void SetBit( uint8_t* value, const uint32_t bit ) {
 	*value |= ( 1u << bit );
 }
 
-void SetBit( uint16_t* value, const uint32_t bit ) {
+inline void SetBit( uint16_t* value, const uint32_t bit ) {
 	*value |= ( 1u << bit );
 }
 
-void SetBit( uint32_t* value, const uint32_t bit ) {
+inline void SetBit( uint32_t* value, const uint32_t bit ) {
 	*value |= ( 1u << bit );
 }
 
-void SetBit( uint64_t* value, const uint32_t bit ) {
+inline void SetBit( uint64_t* value, const uint32_t bit ) {
 	*value |= ( 1ull << bit );
 }
 
-[[nodiscard]] uint8_t UnSetBit( const uint8_t value, const uint32_t bit ) {
+[[nodiscard]] inline uint8_t UnSetBit( const uint8_t value, const uint32_t bit ) {
 	return value & ~( 1u << bit );
 }
 
-[[nodiscard]] uint16_t UnSetBit( const uint16_t value, const uint32_t bit ) {
+[[nodiscard]] inline uint16_t UnSetBit( const uint16_t value, const uint32_t bit ) {
 	return value & ~( 1u << bit );
 }
 
-[[nodiscard]] uint32_t UnSetBit( const uint32_t value, const uint32_t bit ) {
+[[nodiscard]] inline uint32_t UnSetBit( const uint32_t value, const uint32_t bit ) {
 	return value & ~( 1u << bit );
 }
 
-[[nodiscard]] uint64_t UnSetBit( const uint64_t value, const uint32_t bit ) {
+[[nodiscard]] inline uint64_t UnSetBit( const uint64_t value, const uint32_t bit ) {
 	return value & ~( 1ull << bit );
 }
 
-void UnSetBit( uint8_t* value, const uint32_t bit ) {
+inline void UnSetBit( uint8_t* value, const uint32_t bit ) {
 	*value &= ~( 1u << bit );
 }
 
-void UnSetBit( uint16_t* value, const uint32_t bit ) {
+inline void UnSetBit( uint16_t* value, const uint32_t bit ) {
 	*value &= ~( 1u << bit );
 }
 
-void UnSetBit( uint32_t* value, const uint32_t bit ) {
+inline void UnSetBit( uint32_t* value, const uint32_t bit ) {
 	*value &= ~( 1u << bit );
 }
 
-void UnSetBit( uint64_t* value, const uint32_t bit ) {
+inline void UnSetBit( uint64_t* value, const uint32_t bit ) {
 	*value &= ~( 1ull << bit );
 }
 
-int CompareBit( const uint8_t lhs, const uint8_t rhs, const uint32_t bit ) {
+inline int CompareBit( const uint8_t lhs, const uint8_t rhs, const uint32_t bit ) {
 	const uint8_t lhsBit = lhs & ( 1u << bit );
 	const uint8_t rhsBit = rhs & ( 1u << bit );
 	return lhsBit < rhsBit ? -1 : ( lhsBit > rhsBit ? 1 : 0 );
 }
 
-int CompareBit( const uint16_t lhs, const uint16_t rhs, const uint32_t bit ) {
+inline int CompareBit( const uint16_t lhs, const uint16_t rhs, const uint32_t bit ) {
 	const uint16_t lhsBit = lhs & ( 1u << bit );
 	const uint16_t rhsBit = rhs & ( 1u << bit );
 	return lhsBit < rhsBit ? -1 : ( lhsBit > rhsBit ? 1 : 0 );
 }
 
-int CompareBit( const uint32_t lhs, const uint32_t rhs, const uint32_t bit ) {
+inline int CompareBit( const uint32_t lhs, const uint32_t rhs, const uint32_t bit ) {
 	const uint32_t lhsBit = lhs & ( 1u << bit );
 	const uint32_t rhsBit = rhs & ( 1u << bit );
 	return lhsBit < rhsBit ? -1 : ( lhsBit > rhsBit ? 1 : 0 );
 }
 
-int CompareBit( const uint64_t lhs, const uint64_t rhs, const uint32_t bit ) {
+inline int CompareBit( const uint64_t lhs, const uint64_t rhs, const uint32_t bit ) {
 	const uint64_t lhsBit = lhs & ( 1ull << bit );
 	const uint64_t rhsBit = rhs & ( 1ull << bit );
 	return lhsBit < rhsBit ? -1 : ( lhsBit > rhsBit ? 1 : 0 );
 }
 
-const bool BitSet( const uint8_t value, const uint32_t bit ) {
+inline const bool BitSet( const uint8_t value, const uint32_t bit ) {
 	return value & ( 1u << bit );
 }
 
-const bool BitSet( const uint16_t value, const uint32_t bit ) {
+inline const bool BitSet( const uint16_t value, const uint32_t bit ) {
 	return value & ( 1u << bit );
 }
 
-const bool BitSet( const uint32_t value, const uint32_t bit ) {
+inline const bool BitSet( const uint32_t value, const uint32_t bit ) {
 	return value & ( 1u << bit );
 }
 
-const bool BitSet( const uint64_t value, const uint32_t bit ) {
+inline const bool BitSet( const uint64_t value, const uint32_t bit ) {
 	return value & ( 1ull << bit );
 }
 
-uint32_t FindMZeroBit( uint64_t value ) {
+inline uint32_t FindMZeroBit( uint64_t value ) {
 	if ( value == UINT64_MAX ) {
 		return 64;
 	}
@@ -279,7 +279,7 @@ uint32_t FindMZeroBit( uint64_t value ) {
 	return bit - 1;
 }
 
-uint32_t FindZeroBitFast( const uint64_t value ) {
+inline uint32_t FindZeroBitFast( const uint64_t value ) {
 	const uint32_t bit = FindLSB( value );
 	return bit ? bit - 1 : FindLSB( ~value ) - 1;
 }
