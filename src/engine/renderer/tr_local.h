796e130b3098ad267bb22a271adc5979dcdfae85
slipher <slipher@protonmail.com>
2025-07-12 23:25:26 -0300

    Nuke SHADER_3D_(DYNAMIC_STATIC) distinction
    SHADER_3D_DYNAMIC and SHADER_3D_STATIC were two options for implicitly
    creating a shader from an image that lacks a text shader. Merge them
    into a single SHADER_3D.
    
    The only distinction between the two was using CGEN_IDENTITY vs.
    CGEN_IDENTITY_LIGHTING. This only makes a difference if r_overbrightQ3
    is on. But even for emulating Quake 3 the code was wrong: it requested
    CGEN_IDENTITY_LIGHTING, which cancels overbright, for models. Models use
    the light grid which *should* receive the overbright factor. Actual ioq3
    uses CGEN_LIGHTING_DIFFUSE, which we no longer have, for those models.

--- src/engine/renderer/tr_local.h
+++ src/engine/renderer/tr_local.h
@@ -1208,8 +1208,7 @@ enum class ssaoMode {
 	enum class shaderType_t
 	{
 	  SHADER_2D, // surface material: shader is for 2D rendering (like GUI elements)
-	  SHADER_3D_DYNAMIC, // surface material: shader is for cGen diffuseLighting lighting
-	  SHADER_3D_STATIC, // surface material: pre-lit triangle models
+	  SHADER_3D,
 	};
 
 	struct shader_t
