d4cf29b5a10f4eab6bdc72ad9f08ca91fc447aeb
slipher <slipher@protonmail.com>
2025-07-16 02:23:14 -0300

    Allow shaders to be re-registered with different flags
    Allow instantiating multiple versions of a q3shader if it was registered
    with different RSF_* flags each time. This will make it so that
    registering stuff in a different order will not give different behavior.
    
    NUKE shaderType_t and replace it with a new flag RSF_IMPLICIT_3D that
    replicates the behavior of the old shaderType_t::3D. Previously, shaders
    could be registered multiple times but only based on a differing
    shaderType_t.

--- src/engine/renderer/tr_local.h
+++ src/engine/renderer/tr_local.h
@@ -1052,6 +1052,10 @@ enum class ssaoMode {
 	struct Material;
 	struct MaterialSurface;
 
+	// Use 3D-appropriate defaults if it is an implicit shader without shader text.
+	// TODO(0.56): move to the public RegisterShaderFlags_t interface
+#define RSF_IMPLICIT_3D ( BIT( 30 ) )
+
 	using stageRenderer_t = void(*)(shaderStage_t *);
 	using stageShaderBuildMarker_t = void(*)(const shaderStage_t*);
 	using surfaceDataUpdater_t = void(*)(uint32_t*, shaderStage_t*, bool, bool, bool);
@@ -1206,16 +1210,10 @@ enum class ssaoMode {
 		float  depthForOpaque;
 	};
 
-	enum class shaderType_t
-	{
-	  SHADER_2D, // surface material: shader is for 2D rendering (like GUI elements)
-	  SHADER_3D,
-	};
-
 	struct shader_t
 	{
 		char         name[ MAX_QPATH ]; // game path, including extension
-		shaderType_t type;
+		int registerFlags; // RSF_
 
 		int          index; // this shader == tr.shaders[index]
 		int          sortedIndex; // this shader == tr.sortedShaders[sortedIndex]
@@ -3104,7 +3102,7 @@ inline bool checkGLErrors()
 	qhandle_t RE_RegisterShader( const char *name, int flags );
 	qhandle_t RE_RegisterShaderFromImage( const char *name, image_t *image );
 
-	shader_t  *R_FindShader( const char *name, shaderType_t type, int flags );
+	shader_t  *R_FindShader( const char *name, int flags );
 	shader_t  *R_GetShaderByHandle( qhandle_t hShader );
 	shader_t  *R_FindShaderByName( const char *name );
 	const char *RE_GetShaderNameFromHandle( qhandle_t shader );
