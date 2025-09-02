c4b6dd33f3e3e8cb10740ea11410d4d893aa554b
slipher <slipher@protonmail.com>
2025-09-02 16:27:11 -0500

    windows

--- tools/crash_test.py
+++ tools/crash_test.py
@@ -1,31 +1,11 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
+import shutil
 import subprocess
 import sys
 import traceback
-
-DAEMON_DIR = '/unv/Unvanquished/daemon'
-BREAKPAD_DIR = '/unv/Unvanquished/daemon/libs/breakpad'
-GAME_BUILD_DIR = '.'
-ARCH = 'amd64'
-
-SYMBOLIZE = os.path.join(BREAKPAD_DIR, "symbolize.py")
-STACKWALK = os.path.join(BREAKPAD_DIR, "src/processor/minidump_stackwalk")
-
-DAEMON_TTYCLIENT = os.path.join(GAME_BUILD_DIR, 'daemon-tty')
-DAEMON_SERVER = os.path.join(GAME_BUILD_DIR, 'daemonded')
-TEMP_DIR = os.path.join(GAME_BUILD_DIR, "crashtest-tmp")
-os.makedirs(TEMP_DIR, exist_ok=True)
-SYMBOL_DIR = os.path.join(TEMP_DIR, "symbols")
-os.makedirs(SYMBOL_DIR, exist_ok=True)
-HOMEPATH = os.path.join(TEMP_DIR, "homepath")
-
-
-
-assert os.path.isfile(DAEMON_SERVER)
-assert os.path.isfile(STACKWALK)
-
 
 class CrashTest:
     def __init__(self, name):
@@ -38,10 +18,13 @@ class CrashTest:
     def End(self):
         print(f"==={self.status}: {self.name}===")
 
-    def Verify(self, cond, reason):
+    def Verify(self, cond, reason=None):
         if not cond:
-            print(f"FAILURE: {reason}")
+            if reason is not None:
+                print(f"FAILURE: {reason}")
             self.status = "FAILED"
+            if GIVE_UP:
+                raise Exception("Giving up on first failure")
 
     def Go(self):
         self.Begin()
@@ -53,80 +36,147 @@ class CrashTest:
         self.End()
         return self.status == "PASSED"
 
-class NaclCrashTest(CrashTest):
-    def __init__(self, engine, tprefix, fault):
-        super().__init__(f"nacl.{fault}")
+class BreakpadCrashTest(CrashTest):
+    def __init__(self, module, engine, tprefix, fault):
+        super().__init__(module + "." + fault)
         self.engine = engine
         self.tprefix = tprefix
         self.fault = fault
+        self.dir = os.path.join(TEMP_DIR, self.name)
+        try:
+            shutil.rmtree(self.dir)
+        except FileNotFoundError:
+            pass
+        os.makedirs(self.dir)
 
     def Do(self):
         print("Running daemon...")
-        p = subprocess.run([self.engine, "-set", "vm.sgame.type", "1",
+        p = subprocess.run([self.engine,
+                            "-set", "vm.sgame.type", "1",
                             "-set", "vm.cgame.type", "1",
-                        #"-set", "logs.level.fs", "warning",
-                        "-set", "sv_fps", "1000",
-                        "-set", "common.framerate.max", "0",
-                        #"-set", "server.private", "2",
-                        #"-set", "sv_networkScope", "0",
-                        "-set", "net_enabled", "0",
-                        "-set", "common.framerate.max", "0",
-                        "-pakpath", "/unv/Unvanquished/pkg",
-                        #"-homepath", HOMEPATH,
-                        #"-pakpath", os.path.join(DAEMON_DIR, "pkg"),
-                        #"-set", "fs_basepak", "testdata",
-                        "+devmap plat23",
-                        "+delay 20f echo CRASHTEST_BEGIN",
-                        f"+delay 20f {self.tprefix}injectFault", self.fault,
-                        "+delay 20f echo CRASHTEST_END",
-                        "+delay 40f quit"],
-        stderr=subprocess.PIPE,      check=False)
-        log = [s.strip() for s in p.stderr.decode("utf8").splitlines()]
-        i = log.index("CRASHTEST_BEGIN")
-        #j = log.index("CRASHTEST_END")
-        j = len(log) - 1
-        # TODO expected vs. actual Warn's
-        DUMP_PREFIX = "Wrote crash dump to "
-        dumps = [l for l in log if l.startswith(DUMP_PREFIX)]
-        assert len(dumps) == 1, "Daemon log contains 1 crash dump"
-        dump = dumps[0][len(DUMP_PREFIX):]
-        sw_out = os.path.join(TEMP_DIR, self.tprefix + self.fault + ".stackwalk.log")
+                            "-set", "sv_fps", "1000",
+                            "-set", "common.framerate.max", "0",
+                            "-set", "client.errorPopup", "0",
+                            "-set", "server.private", "2",
+                            "-set", "net_enabled", "0",
+                            "-set", "common.framerate.max", "0",
+                            "-set", "cg_navgenOnLoad", "0",
+                            "-homepath", self.dir,
+                            *DAEMON_USER_ARGS,
+                            *["+devmap plat23"] * (self.tprefix == "sgame."),
+                            "+delay 20f echo CRASHTEST_BEGIN",
+                            f"+delay 20f {self.tprefix}injectFault", self.fault,
+                            "+delay 20f echo CRASHTEST_END",
+                            "+delay 40f quit"],
+                            stderr=subprocess.PIPE, check=bool(self.tprefix))
+        dumps = os.listdir(os.path.join(self.dir, "crashdump"))
+        assert len(dumps) == 1, dumps
+        dump = os.path.join(self.dir, "crashdump", dumps[0])
+        sw_out = os.path.join(TEMP_DIR, self.name + "_stackwalk.log")
         with open(sw_out, "a+") as sw_f:
             print(f"Extracting stack trace to '{sw_out}'...")
             sw_f.truncate()
-            subprocess.run([STACKWALK, dump, SYMBOL_DIR], check=True, stdout=sw_f, stderr=subprocess.STDOUT)
+            subprocess.run(Virtualize([os.path.join(BREAKPAD_DIR, "src/processor/minidump_stackwalk"), dump, SYMBOL_DIR]), check=True, stdout=sw_f, stderr=subprocess.STDOUT)
             sw_f.seek(0)
             sw = sw_f.read()
         TRACE_FUNC = "InjectFaultCmd::Run"
         self.Verify(TRACE_FUNC in sw, "function names not found in trace (did you build with symbols?)")
 
-def DoModule(module):
-    try:
-        engine = DAEMON_TTYCLIENT
+def Virtualize(cmdline):
+    bin, *args = cmdline
+    if EXE:
+        bin2 = bin.replace("\\", "/")
+        if bin2.startswith("//wsl.localhost/"):
+            parts = bin2.split("/")
+            vm = parts[3]
+            path = "/" + "/".join(parts[4:])
+            return ["wsl", "-d", vm, "--", path] + args
+        if bin.endswith(".py"):
+            return [sys.executable] + cmdline
+    return cmdline
+
+def ModulePath(module):
+    base = {
+        "dummyapp" : "dummyapp" + EXE,
+        "server": "daemonded" + EXE,
+        "ttyclient": "daemon-tty" + EXE,
+        "client": "daemon" + EXE,
+        "sgame": f"sgame-{NACL_ARCH}.nexe",
+        "cgame": f"cgame-{NACL_ARCH}.nexe",
+    }[module]
+    return os.path.join(GAME_BUILD_DIR, base)
+
+class ModuleCrashTests(CrashTest):
+    def __init__(self, module, engine=None):
+        super().__init__(module)
+        self.engine = engine
+
+    def Do(self):
+        module = self.name
         if module == "sgame":
-            target = os.path.join(GAME_BUILD_DIR, f"sgame-{ARCH}.nexe")
+            eng = self.engine or "server"
             tprefix = "sgame."
         elif module == "cgame":
-            target = os.path.join(GAME_BUILD_DIR, f"cgame-{ARCH}.nexe")
             tprefix = "cgame."
-        elif module == "server":
+            eng = self.engine or "ttyclient"
+        else:
             tprefix = ""
-            engine = target = DAEMON_SERVER
+            eng = module
+        engine = ModulePath(eng)
+        target = ModulePath(module)
 
         assert os.path.isfile(target), target
+        assert os.path.isfile(engine), engine
         print(f"Symbolizing '{target}'...")
-        subprocess.check_call([sys.executable, SYMBOLIZE, "--symbol-directory", SYMBOL_DIR, target])
+        subprocess.check_call(Virtualize([os.path.join(BREAKPAD_DIR, "symbolize.py"),
+                               "--symbol-directory", SYMBOL_DIR, target]))
 
-        return (True
-            & NaclCrashTest(DAEMON_TTYCLIENT, tprefix, "exception").Go()
-            & NaclCrashTest(DAEMON_TTYCLIENT, tprefix, "throw").Go()
-            & NaclCrashTest(DAEMON_TTYCLIENT, tprefix, "abort").Go()
-            & NaclCrashTest(DAEMON_TTYCLIENT, tprefix, "segfault").Go())
-    except Exception:
-        raise
-        return False
+        self.Verify(BreakpadCrashTest(module, engine, tprefix, "segfault").Go())
+        if tprefix or not EXE:
+            # apparently abort() is caught on Linux but not Windows
+            self.Verify(BreakpadCrashTest(module, engine, tprefix, "abort").Go())
+        if tprefix:
+            self.Verify(BreakpadCrashTest(module, engine, tprefix, "exception").Go())
+            self.Verify(BreakpadCrashTest(module, engine, tprefix, "throw").Go())
 
-#passed = DoModule("sgame") & DoModule("cgame")
-passed = DoModule("server")
+def ArgParser(usage=None):
+    ap = argparse.ArgumentParser(usage=usage)
+    ap.add_argument("--breakpad-dir", type=str, default=BREAKPAD_DIR, help=r"Path to Breakpad repo containing built dump_syms and stackwalk binaries. It may be a \\wsl.localhost\ path on Windows hosts in order to symbolize NaCl.")
+    ap.add_argument("--give-up", action="store_true", help="Stop after first test failure")
+    ap.add_argument("--nacl-arch", type=str, choices=["amd64", "i686", "armhf"], default="amd64") # TODO auto-detect?
+    ap.add_argument("module", nargs="*", choices=[
+        "dummyapp", "server", "ttyclient", "client",
+        "cgame", "ttyclient:cgame", "client:cgame",
+        "sgame", "server:sgame", "ttyclient:sgame", "client:sgame"])
+    return ap
+
+BREAKPAD_DIR = os.path.abspath(os.path.join(
+    os.path.dirname(os.path.realpath(__file__)), "../libs/breakpad"))
+GAME_BUILD_DIR = '.' # WSL calls rely on relative paths
+TEMP_DIR = os.path.join(GAME_BUILD_DIR, "crashtest-tmp")
+SYMBOL_DIR = os.path.join(TEMP_DIR, "symbols")
+
+os.makedirs(TEMP_DIR, exist_ok=True)
+os.makedirs(SYMBOL_DIR, exist_ok=True)
+
+if os.name == "nt":
+    EXE = '.exe'
+else:
+    EXE = ""
+
+ap = ArgParser(usage=ArgParser().format_usage().rstrip().removeprefix("usage: ")
+                + " [--daemon-args ARGS...]")
+ap.add_argument("--daemon-args", nargs=argparse.REMAINDER, default=[],
+                help="Extra arguments for Daemon (e.g. -pakpath)")
+pa = ap.parse_args(sys.argv[1:])
+BREAKPAD_DIR = pa.breakpad_dir
+GIVE_UP = pa.give_up
+DAEMON_USER_ARGS = pa.daemon_args
+NACL_ARCH = pa.nacl_arch
+modules = pa.module or ["server", "ttyclient", "sgame", "cgame"]
+passed = True
+for module in modules:
+    passed &= ModuleCrashTests(*module.split(":")[::-1]).Go()
+    if not passed and GIVE_UP:
+        break
 sys.exit(1 - passed)
-
