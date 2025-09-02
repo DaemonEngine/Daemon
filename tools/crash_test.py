369277c40fef2096314d47ceed2e2d05a774ffed
slipher <slipher@protonmail.com>
2025-09-02 07:18:20 -0500

    blah

--- tools/crash_test.py
+++ tools/crash_test.py
@@ -21,19 +21,11 @@ SYMBOL_DIR = os.path.join(TEMP_DIR, "symbols")
 os.makedirs(SYMBOL_DIR, exist_ok=True)
 HOMEPATH = os.path.join(TEMP_DIR, "homepath")
 
-dummy = False
-if dummy:
-    GAMELOGIC_NACL = os.path.join(GAME_BUILD_DIR, f"cgame-{ARCH}.nexe")
-else:
-    GAMELOGIC_NACL = os.path.join(GAME_BUILD_DIR, f"sgame-{ARCH}.nexe")
 
 
-assert os.path.isfile(GAMELOGIC_NACL), GAMELOGIC_NACL
 assert os.path.isfile(DAEMON_SERVER)
 assert os.path.isfile(STACKWALK)
 
-print(f"Symbolizing '{GAMELOGIC_NACL}'...")
-subprocess.check_call([sys.executable, SYMBOLIZE, "--symbol-directory", SYMBOL_DIR, GAMELOGIC_NACL])
 
 class CrashTest:
     def __init__(self, name):
@@ -42,8 +34,6 @@ class CrashTest:
     def Begin(self):
         self.status = "PASSED"
         print(f"===RUNNING: {self.name}===")
-        self.tmpdir = os.path.join(TEMP_DIR, self.name)
-        os.makedirs(self.tmpdir, exist_ok=True)
 
     def End(self):
         print(f"==={self.status}: {self.name}===")
@@ -64,37 +54,43 @@ class CrashTest:
         return self.status == "PASSED"
 
 class NaclCrashTest(CrashTest):
-    def __init__(self, fault):
+    def __init__(self, engine, tprefix, fault):
         super().__init__(f"nacl.{fault}")
+        self.engine = engine
+        self.tprefix = tprefix
         self.fault = fault
 
     def Do(self):
         print("Running daemon...")
-        p = subprocess.run([DAEMON_SERVER, "-set", "vm.sgame.type", "1",
+        p = subprocess.run([self.engine, "-set", "vm.sgame.type", "1",
+                            "-set", "vm.cgame.type", "1",
                         #"-set", "logs.level.fs", "warning",
                         "-set", "sv_fps", "1000",
+                        "-set", "common.framerate.max", "0",
                         #"-set", "server.private", "2",
                         #"-set", "sv_networkScope", "0",
                         "-set", "net_enabled", "0",
                         "-set", "common.framerate.max", "0",
+                        "-pakpath", "/unv/Unvanquished/pkg",
                         #"-homepath", HOMEPATH,
                         #"-pakpath", os.path.join(DAEMON_DIR, "pkg"),
                         #"-set", "fs_basepak", "testdata",
                         "+devmap plat23",
                         "+delay 20f echo CRASHTEST_BEGIN",
-                        "+delay 20f sgame.injectFault", self.fault,
+                        f"+delay 20f {self.tprefix}injectFault", self.fault,
                         "+delay 20f echo CRASHTEST_END",
                         "+delay 40f quit"],
-        stderr=subprocess.PIPE)
+        stderr=subprocess.PIPE,      check=False)
         log = [s.strip() for s in p.stderr.decode("utf8").splitlines()]
         i = log.index("CRASHTEST_BEGIN")
-        j = log.index("CRASHTEST_END")
+        #j = log.index("CRASHTEST_END")
+        j = len(log) - 1
         # TODO expected vs. actual Warn's
         DUMP_PREFIX = "Wrote crash dump to "
         dumps = [l for l in log if l.startswith(DUMP_PREFIX)]
         assert len(dumps) == 1, "Daemon log contains 1 crash dump"
         dump = dumps[0][len(DUMP_PREFIX):]
-        sw_out = os.path.join(self.tmpdir, "stackwalk.log")
+        sw_out = os.path.join(TEMP_DIR, self.tprefix + self.fault + ".stackwalk.log")
         with open(sw_out, "a+") as sw_f:
             print(f"Extracting stack trace to '{sw_out}'...")
             sw_f.truncate()
@@ -104,10 +100,33 @@ class NaclCrashTest(CrashTest):
         TRACE_FUNC = "InjectFaultCmd::Run"
         self.Verify(TRACE_FUNC in sw, "function names not found in trace (did you build with symbols?)")
 
-passed = (True
-    & NaclCrashTest("exception").Go()
-    & NaclCrashTest("throw").Go()
-    & NaclCrashTest("abort").Go()
-    & NaclCrashTest("segfault").Go())
+def DoModule(module):
+    try:
+        engine = DAEMON_TTYCLIENT
+        if module == "sgame":
+            target = os.path.join(GAME_BUILD_DIR, f"sgame-{ARCH}.nexe")
+            tprefix = "sgame."
+        elif module == "cgame":
+            target = os.path.join(GAME_BUILD_DIR, f"cgame-{ARCH}.nexe")
+            tprefix = "cgame."
+        elif module == "server":
+            tprefix = ""
+            engine = target = DAEMON_SERVER
+
+        assert os.path.isfile(target), target
+        print(f"Symbolizing '{target}'...")
+        subprocess.check_call([sys.executable, SYMBOLIZE, "--symbol-directory", SYMBOL_DIR, target])
+
+        return (True
+            & NaclCrashTest(DAEMON_TTYCLIENT, tprefix, "exception").Go()
+            & NaclCrashTest(DAEMON_TTYCLIENT, tprefix, "throw").Go()
+            & NaclCrashTest(DAEMON_TTYCLIENT, tprefix, "abort").Go()
+            & NaclCrashTest(DAEMON_TTYCLIENT, tprefix, "segfault").Go())
+    except Exception:
+        raise
+        return False
+
+#passed = DoModule("sgame") & DoModule("cgame")
+passed = DoModule("server")
 sys.exit(1 - passed)
 
