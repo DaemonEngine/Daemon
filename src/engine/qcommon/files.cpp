a899a33a24ebad8304f89a85f9b9301c2c1df061
Thomas Debesse <dev@illwieckz.net>
2025-10-04 08:41:16 +0200

    no sv_paks

--- src/engine/qcommon/files.cpp
+++ src/engine/qcommon/files.cpp
@@ -626,6 +626,8 @@ const char* FS_LoadedPaks()
 	static char info[BIG_INFO_STRING];
 	info[0] = '\0';
 	for (const FS::LoadedPakInfo& x: FS::PakPath::GetLoadedPaks()) {
+		if (x.type == FS::pakType_t::PAK_BUILTIN)
+			continue;
 		if (!x.pathPrefix.empty())
 			continue;
 		if (info[0])
