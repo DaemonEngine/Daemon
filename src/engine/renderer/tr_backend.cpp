7d59d7ce4a8463bfeea0aceb85f453988b83a0ab
VReaperV <reaper9977@gmail.com>
2026-01-12 04:23:23 +0300

    Only update PushBuffer if any uniform has actually changed
    Also mutualises `uniformFirewall` and `currentValue` and removes a bunch of unneeded code.

--- src/engine/renderer/tr_backend.cpp
+++ src/engine/renderer/tr_backend.cpp
@@ -2884,8 +2884,6 @@ static void SetFrameUniforms() {
 
 	GLIMP_LOGCOMMENT( "--- SetFrameUniforms ---" );
 
-	uint32_t* data = pushBuffer.MapGlobalUniformData( GLUniform::FRAME );
-
 	globalUBOProxy->SetUniform_blurVec( tr.refdef.blurVec );
 	globalUBOProxy->SetUniform_numLights( tr.refdef.numLights );
 
@@ -2906,6 +2904,11 @@ static void SetFrameUniforms() {
 		materialSystem.SetFrameUniforms();
 	}
 
+	if ( !globalUBOProxy->uniformsUpdated ) {
+		return;
+	}
+
+	uint32_t* data = pushBuffer.MapGlobalUniformData( GLUniform::FRAME );
 	globalUBOProxy->WriteUniformsToBuffer( data, GLShader::PUSH, GLUniform::FRAME );
 
 	pushBuffer.PushGlobalUniforms();
