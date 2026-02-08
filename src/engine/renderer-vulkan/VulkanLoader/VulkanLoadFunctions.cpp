da911b5c40759cd6e816501d2e67a90ae96e8aac
VReaperV <reaper9977@gmail.com>
2026-02-08 03:13:27 +0300

    Update VulkanLoadFunctions

--- src/engine/renderer-vulkan/VulkanLoader/VulkanLoadFunctions.cpp
+++ src/engine/renderer-vulkan/VulkanLoader/VulkanLoadFunctions.cpp
@@ -1,45 +1,45 @@
-// Auto-generated, do not modify
-
-#ifdef _MSC_VER
-	#include <windows.h>
-#else
-	#include <dlfcn.h>
-#endif
-
-#include "Vulkan.h"
-
-#include "VulkanLoadFunctions.h"
-
-#ifdef _MSC_VER
-	HMODULE libVulkan;
-#else
-	void*   libVulkan;
-#endif
-
-void VulkanLoaderInit() {
-	#ifdef _MSC_VER
-		libVulkan = LoadLibrary( "vulkan-1.dll" );
-		vkGetInstanceProcAddr = ( PFN_vkGetInstanceProcAddr ) GetProcAddress( libVulkan, "vkGetInstanceProcAddr" );
-	#else
-		libVulkan = dlopen( "libvulkan.so", RTLD_NOW );
-		vkGetInstanceProcAddr = dlsym( libVulkan, "vkGetInstanceProcAddr" );
-	#endif
-
-	vkEnumerateInstanceVersion = ( PFN_vkEnumerateInstanceVersion ) vkGetInstanceProcAddr( nullptr, "vkEnumerateInstanceVersion" );
-
-	vkEnumerateInstanceExtensionProperties = ( PFN_vkEnumerateInstanceExtensionProperties ) vkGetInstanceProcAddr( nullptr, "vkEnumerateInstanceExtensionProperties" );
-
-	vkEnumerateInstanceLayerProperties = ( PFN_vkEnumerateInstanceLayerProperties ) vkGetInstanceProcAddr( nullptr, "vkEnumerateInstanceLayerProperties" );
-
-	vkCreateInstance = ( PFN_vkCreateInstance ) vkGetInstanceProcAddr( nullptr, "vkCreateInstance" );
-}
-
-void VulkanLoaderFree() {
-	#ifdef _MSC_VER
-		FreeLibrary( libVulkan );
-	#else
-		dlclose( libVulkan );
-	#endif
+// Auto-generated, do not modify
+
+#ifdef _MSC_VER
+	#include <windows.h>
+#else
+	#include <dlfcn.h>
+#endif
+
+#include "Vulkan.h"
+
+#include "VulkanLoadFunctions.h"
+
+#ifdef _MSC_VER
+	HMODULE libVulkan;
+#else
+	void*   libVulkan;
+#endif
+
+void VulkanLoaderInit() {
+	#ifdef _MSC_VER
+		libVulkan = LoadLibrary( "vulkan-1.dll" );
+		vkGetInstanceProcAddr = ( PFN_vkGetInstanceProcAddr ) GetProcAddress( libVulkan, "vkGetInstanceProcAddr" );
+	#else
+		libVulkan = dlopen( "libvulkan.so", RTLD_NOW );
+		vkGetInstanceProcAddr = dlsym( libVulkan, "vkGetInstanceProcAddr" );
+	#endif
+
+	vkEnumerateInstanceVersion = ( PFN_vkEnumerateInstanceVersion ) vkGetInstanceProcAddr( nullptr, "vkEnumerateInstanceVersion" );
+
+	vkEnumerateInstanceExtensionProperties = ( PFN_vkEnumerateInstanceExtensionProperties ) vkGetInstanceProcAddr( nullptr, "vkEnumerateInstanceExtensionProperties" );
+
+	vkEnumerateInstanceLayerProperties = ( PFN_vkEnumerateInstanceLayerProperties ) vkGetInstanceProcAddr( nullptr, "vkEnumerateInstanceLayerProperties" );
+
+	vkCreateInstance = ( PFN_vkCreateInstance ) vkGetInstanceProcAddr( nullptr, "vkCreateInstance" );
+}
+
+void VulkanLoaderFree() {
+	#ifdef _MSC_VER
+		FreeLibrary( libVulkan );
+	#else
+		dlclose( libVulkan );
+	#endif
 }
 
 void VulkanLoadInstanceFunctions( VkInstance instance ) {
