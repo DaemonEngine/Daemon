3182c8c2f65ad5c54958a9ae75cc489ff05c661c
VReaperV <reaper9977@gmail.com>
2026-02-08 03:09:45 +0300

    Move header processing to Globals

--- cmake/DaemonVulkan/VulkanHeaders/Globals.py
+++ cmake/DaemonVulkan/VulkanHeaders/Globals.py
@@ -5,8 +5,72 @@ def init():
     global functionLoadInstanceText
     global functionLoadDeviceText
     
-    headerText = ''
-    functionDefinitionsText = ''
+    headerText               = ""
+    functionDefinitionsText  = ""
 
-    functionLoadInstanceText = ''
-    functionLoadDeviceText   = ''
+    functionLoadInstanceText = ""
+    functionLoadDeviceText   = ""
+
+def GenerateHeaders( inputDir, outputDir, mode, define ):
+        headerStart       = ""
+        functionLoadStart = ""
+
+        if mode == "w":
+            with open( inputDir + "Vulkan.h", mode = "r", encoding = "utf-8", newline = "\n" ) as inp:
+                headerStart = inp.read()
+                
+            with open( inputDir + "VulkanLoadFunctions.cpp", mode = "r", encoding = "utf-8", newline = "\n" ) as inp:
+                functionLoadStart = inp.read()
+
+        outputDir = outputDir.rstrip( "/" ).rsplit( "/", 1 )[0] + "/"
+        
+        indent    = "\t" if define else ""
+        
+        with open( "FunctionDecls.h", mode = mode, encoding = "utf-8", newline = "\n" ) as out:
+            if define:
+                out.write( "#if defined( " + define + " )\n" )
+            
+            out.write( headerStart + indent + headerText )
+
+            if define:
+                out.write( "#endif\n\n" )
+        
+        with open( outputDir + "Vulkan.cpp", mode = mode, encoding = "utf-8", newline = "\n" ) as out:
+            if mode == "w":
+                out.write( "// Auto-generated, do not modify\n\n" )
+                out.write( "#include \"Vulkan.h\"\n\n" )
+            
+            if define:
+                out.write( "#if defined( " + define + " )\n" )
+            
+            out.write( indent + functionDefinitionsText )
+
+            if define:
+                out.write( "#endif\n\n" )
+        
+        #with open( outputDir + "VulkanLoadFunctions.cpp", mode = mode, encoding = "utf-8", newline = "\n" ) as out:
+        #    out.write( functionLoadStart )
+        #    out.write( "\n\nvoid VulkanLoadInstanceFunctions( VkInstance instance ) {\n" )
+        #    out.write( Globals.functionLoadInstanceText )
+        #    out.write( "}\n\n" )
+        #    out.write( "void VulkanLoadDeviceFunctions( VkDevice device ) {\n" )
+        #    out.write( Globals.functionLoadDeviceText )
+        #    out.write( "}" )
+        
+        with open( "FunctionLoaderInstance.cpp", mode = mode, encoding = "utf-8", newline = "\n" ) as out:
+            if define:
+                out.write( "#if defined( " + define + " )\n" )
+            
+            out.write( functionLoadInstanceText )
+
+            if define:
+                out.write( "#endif\n\n" )
+        
+        with open( "FunctionLoaderDevice.cpp", mode = mode, encoding = "utf-8", newline = "\n" ) as out:
+            if define:
+                out.write( "#if defined( " + define + " )\n" )
+            
+            out.write( functionLoadDeviceText )
+
+            if define:
+                out.write( "#endif\n\n" )
--- cmake/DaemonVulkan/VulkanHeaders/reg.py
+++ cmake/DaemonVulkan/VulkanHeaders/reg.py
@@ -1977,15 +1977,6 @@ class Registry:
         #   generated.
         self.gen.logMsg('diag', 'PASS 3: GENERATE INTERFACES FOR FEATURES')
         self.gen.beginFile(self.genOpts)
-        
-        headerStart = ''
-        functionLoadStart = ''
-        if mode == "w":
-            with open( inputDir + 'Vulkan.h', mode = 'r', encoding = 'utf-8', newline = '\n' ) as inp:
-                headerStart = inp.read()
-                
-            with open( inputDir + 'VulkanLoadFunctions.cpp', mode = 'r', encoding = 'utf-8', newline = '\n' ) as inp:
-                functionLoadStart = inp.read()
 
         Globals.init()
 
@@ -2016,58 +2007,7 @@ class Registry:
             self.generateSyncPipeline(self.syncpipelinedict[s])
         self.gen.endFile()
         
-        outputDir = outputDir.rstrip( '/' ).rsplit( '/', 1 )[0] + '/'
-        
-        indent = "\t" if define else ""
-        
-        with open( 'FunctionDecls.h', mode = mode, encoding = 'utf-8', newline = '\n' ) as out:
-            if define:
-                out.write( "#if defined( " + define + " )\n" )
-            
-            out.write( headerStart + indent + Globals.headerText )
-
-            if define:
-                out.write( "#endif\n\n" )
-        
-        with open( outputDir + 'Vulkan.cpp', mode = mode, encoding = 'utf-8', newline = '\n' ) as out:
-            if mode == "w":
-                out.write( '// Auto-generated, do not modify\n\n' )
-                out.write( '#include "Vulkan.h"\n\n' )
-            
-            if define:
-                out.write( "#if defined( " + define + " )\n" )
-            
-            out.write( indent + Globals.functionDefinitionsText )
-
-            if define:
-                out.write( "#endif\n\n" )
-        
-        #with open( outputDir + 'VulkanLoadFunctions.cpp', mode = mode, encoding = 'utf-8', newline = '\n' ) as out:
-        #    out.write( functionLoadStart )
-        #    out.write( '\n\nvoid VulkanLoadInstanceFunctions( VkInstance instance ) {\n' )
-        #    out.write( Globals.functionLoadInstanceText )
-        #    out.write( '}\n\n' )
-        #    out.write( 'void VulkanLoadDeviceFunctions( VkDevice device ) {\n' )
-        #    out.write( Globals.functionLoadDeviceText )
-        #    out.write( '}' )
-        
-        with open( 'FunctionLoaderInstance.cpp', mode = mode, encoding = 'utf-8', newline = '\n' ) as out:
-            if define:
-                out.write( "#if defined( " + define + " )\n" )
-            
-            out.write( Globals.functionLoadInstanceText )
-
-            if define:
-                out.write( "#endif\n\n" )
-        
-        with open( 'FunctionLoaderDevice.cpp', mode = mode, encoding = 'utf-8', newline = '\n' ) as out:
-            if define:
-                out.write( "#if defined( " + define + " )\n" )
-            
-            out.write( Globals.functionLoadDeviceText )
-
-            if define:
-                out.write( "#endif\n\n" )
+        Globals.GenerateHeaders( inputDir, outputDir, mode, define )
 
     def apiReset(self):
         """Reset type/enum/command dictionaries before generating another API.
