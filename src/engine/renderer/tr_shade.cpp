e432d4bb5c1ac8e1779fec7047f303fbbc0e92fd
Thomas Debesse <dev@illwieckz.net>
2026-03-07 00:20:09 +0100

    renderer: make r_FXAA a new-style cvar

--- src/engine/renderer/tr_shade.cpp
+++ src/engine/renderer/tr_shade.cpp
@@ -369,7 +369,7 @@ static void GLSL_InitGPUShadersOrError()
 		gl_ssaoShader->MarkProgramForBuilding();
 	}
 
-	if ( r_FXAA->integer != 0 )
+	if ( r_FXAA.Get() )
 	{
 		gl_shaderManager.LoadShader( gl_fxaaShader );
 
