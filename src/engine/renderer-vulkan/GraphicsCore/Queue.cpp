ef3f0d5b60460cc52c9b24ed9464deddb43bfee1
VReaperV <reaper9977@gmail.com>
2026-03-15 04:14:13 +0300

    Queue clean-up

--- src/engine/renderer-vulkan/GraphicsCore/Queue.cpp
+++ src/engine/renderer-vulkan/GraphicsCore/Queue.cpp
@@ -31,7 +31,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ===========================================================================
 */
-// Queue.h
+// Queue.cpp
 
 #include "../Memory/Array.h"
 
