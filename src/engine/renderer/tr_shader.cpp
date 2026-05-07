DaemonEngine/Daemon

commit bd91b46b54a5d23490d90c8b82667c2306154c39

tr_shader: add the linearColorMap, linearSpecularMap and linearRgbGen keywords and raw variants
- lienarColorMap, linearSpecularMap and linearRgbGen expects
  tr.worldLinearizeTexture. They are meant to be used in assets targeting
  the linear pipeline only.
- rawColorMap, rawSpecularMap and rawRgbGen does the same, but there is no
  colorspace meaning. They are meant to be used when porting legacy assets
  that may be used in the naive pipeline to be rendered as they were before
  for historical purposes, while being ported the hacky way for the linear
  pipeline.

src/engine/renderer/tr_shader.cpp
@@ -1491,15 +1491,22 @@ static bool LoadMap( shaderStage_t *stage, const char *buffer, stageType_t type,
 		to use any shader in UI, so we better take care of more than ST_COLORMAP. */
 		if ( ! ( shader.registerFlags & RSF_2D ) )
 		{
+			stage->convertColorFromSRGB = stage->colorspaceBits & LINEAR_RGBGEN ?
+				convertColorFromSRGB_NOP : stage->convertColorFromSRGB;
+
 			switch ( type )
 			{
 				case stageType_t::ST_COLORMAP:
 				case stageType_t::ST_DIFFUSEMAP:
+					imageParams.bits |= stage->colorspaceBits & LINEAR_COLORMAP ? 0 : IF_SRGB;
+					break;
 				case stageType_t::ST_GLOWMAP:
 				case stageType_t::ST_REFLECTIONMAP:
 				case stageType_t::ST_SKYBOXMAP:
-				case stageType_t::ST_SPECULARMAP:
 					imageParams.bits |= IF_SRGB;
+					break;
+				case stageType_t::ST_SPECULARMAP:
+					imageParams.bits |= stage->colorspaceBits & LINEAR_SPECULARMAP ? 0 : IF_SRGB;
 					break;
 				default:
 					break;
@@ -2095,6 +2102,8 @@ static bool ParseStage( shaderStage_t *stage, const char **text )
 	bool     loadMap = false;
 	bool loadAnimMap = false;
 
+	stage->convertColorFromSRGB = convertColorFromSRGB_NOP;
+
 	memset( delayedStageTextures, 0, sizeof( delayedStageTextures ) );
 	memset( delayedAnimationTextures, 0, sizeof( delayedAnimationTextures ) );
 
@@ -2640,6 +2649,45 @@ static bool ParseStage( shaderStage_t *stage, const char **text )
 			{
 				depthMaskBits = 0;
 			}
+		}
+		else if ( !Q_stricmp( token, "rawRgbGen" ) )
+		{
+			stage->colorspaceBits |= LINEAR_RGBGEN;
+		}
+		else if ( !Q_stricmp( token, "linearRgbGen" ) )
+		{
+			if ( !tr.worldLinearizeTexture )
+			{
+				Log::Warn("Usage of linearRgbGen in naive pipeline, assuming rawRgbGen");
+			}
+
+			stage->colorspaceBits |= LINEAR_RGBGEN;
+		}
+		else if ( !Q_stricmp( token, "rawColorMap" ) )
+		{
+			stage->colorspaceBits |= LINEAR_COLORMAP;
+		}
+		else if ( !Q_stricmp( token, "linearColorMap" ) )
+		{
+			if ( !tr.worldLinearizeTexture )
+			{
+				Log::Warn("Usage of linearColorMap in naive pipeline, assuming rawColorMap");
+			}
+
+			stage->colorspaceBits |= LINEAR_COLORMAP;
+		}
+		else if ( !Q_stricmp( token, "rawSpecularMap" ) )
+		{
+			stage->colorspaceBits |= LINEAR_SPECULARMAP;
+		}
+		else if ( !Q_stricmp( token, "linearSpecularMap" ) )
+		{
+			if ( !tr.worldLinearizeTexture )
+			{
+				Log::Warn("Usage of linearSpecularMap in naive pipeline, assuming rawSpecularMap");
+			}
+
+			stage->colorspaceBits |= LINEAR_SPECULARMAP;
 		}
 		// stage <type>
 		else if ( !Q_stricmp( token, "stage" ) )

src/engine/renderer/tr_init.cpp
@@ -1287,7 +1287,7 @@ ScreenshotCmd screenshotPNGRegistration("screenshotPNG", ssFormat_t::SSF_PNG, "p
 	}
 
 	static float convertFloatFromSRGB_NOP( float f ) { return f; }
-	static Color::Color convertColorFromSRGB_NOP( Color::Color c ) { return c; }
+	Color::Color convertColorFromSRGB_NOP( Color::Color c ) { return c; }
 
 	/*
 	===============

src/engine/renderer/tr_local.h
@@ -979,6 +979,11 @@ enum
 		ALL = BIT( 3 )
 	};
 
+	using floatProcessor_t = float(*)(float);
+	using colorProcessor_t = Color::Color(*)(Color::Color);
+
+	Color::Color convertColorFromSRGB_NOP( Color::Color c );
+
 	struct shaderStage_t
 	{
 		stageType_t     type;
@@ -997,6 +1002,8 @@ enum
 		surfaceDataUpdater_t surfaceDataUpdater;
 		stageShaderBinder_t shaderBinder;
 		stageMaterialProcessor_t materialProcessor;
+
+		colorProcessor_t convertColorFromSRGB;
 
 		textureBundle_t bundle[ MAX_TEXTURE_BUNDLES ];
 
@@ -1021,6 +1028,7 @@ enum
 		Color::Color32Bit constantColor; // for CGEN_CONST and AGEN_CONST
 
 		uint32_t        stateBits; // GLS_xxxx mask
+		uint8_t colorspaceBits; // LINEAR_xxxx mask
 
 		bool            isCubeMap;
 
@@ -1270,6 +1278,12 @@ enum
 	                       | GLS_ALPHAMASK_FALSE,
 
 	  GLS_DEFAULT = GLS_DEPTHMASK_TRUE
+	};
+
+	enum {
+		LINEAR_RGBGEN = ( 1 << 0 ),
+		LINEAR_COLORMAP = ( 1 << 1 ),
+		LINEAR_SPECULARMAP = ( 1 << 2 ),
 	};
 
 // *INDENT-ON*
@@ -2396,9 +2410,6 @@ enum
 		int       w;
 		int       h;
 	};
-
-	using floatProcessor_t = float(*)(float);
-	using colorProcessor_t = Color::Color(*)(Color::Color);
 
 	/*
 	** trGlobals_t

src/engine/renderer/tr_shade.cpp
@@ -1723,7 +1723,7 @@ void Tess_ComputeColor( shaderStage_t *pStage )
 			{
 				tess.svars.color = pStage->constantColor;
 				tess.svars.color.Clamp();
-				tess.svars.color = tr.convertColorFromSRGB( tess.svars.color );
+				tess.svars.color = pStage->convertColorFromSRGB( tess.svars.color );
 				break;
 			}
 
@@ -1733,7 +1733,7 @@ void Tess_ComputeColor( shaderStage_t *pStage )
 				{
 					tess.svars.color = backEnd.currentEntity->e.shaderRGBA;
 					tess.svars.color.Clamp();
-					tess.svars.color = tr.convertColorFromSRGB( tess.svars.color );
+					tess.svars.color = pStage->convertColorFromSRGB( tess.svars.color );
 				}
 				else
 				{
@@ -1749,7 +1749,7 @@ void Tess_ComputeColor( shaderStage_t *pStage )
 				{
 					tess.svars.color = backEnd.currentEntity->e.shaderRGBA;
 					tess.svars.color.Clamp();
-					tess.svars.color = tr.convertColorFromSRGB( tess.svars.color );
+					tess.svars.color = pStage->convertColorFromSRGB( tess.svars.color );
 				}
 				else
 				{
@@ -1782,7 +1782,7 @@ void Tess_ComputeColor( shaderStage_t *pStage )
 
 				tess.svars.color = Color::White * glow;
 				tess.svars.color.Clamp();
-				tess.svars.color = tr.convertColorFromSRGB( tess.svars.color );
+				tess.svars.color = pStage->convertColorFromSRGB( tess.svars.color );
 				break;
 			}
 

commit 6dc2d3f5d9b948eeb22d4ddb9fb24601746b3f48

Fix rgbGen linearization
Regression in bd91b46b54a5d23490d90c8b82667c2306154c39

src/engine/renderer/tr_shader.cpp
@@ -1486,7 +1486,7 @@ static bool LoadMap( shaderStage_t *stage, const char *buffer, stageType_t type,
 		if ( ! ( shader.registerFlags & RSF_2D ) )
 		{
 			stage->convertColorFromSRGB = stage->colorspaceBits & LINEAR_RGBGEN ?
-				convertColorFromSRGB_NOP : stage->convertColorFromSRGB;
+				convertColorFromSRGB_NOP : tr.convertColorFromSRGB;
 
 			switch ( type )
 			{