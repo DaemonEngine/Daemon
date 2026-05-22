608eaeabb09b9a8f2210a54a57bbb267ab1d3767
Reaper <reaper9977@gmail.com>
2026-05-22 04:05:45 +0300

    Add Allocator, Array, BitStream, IteratorSeq

--- src/engine/renderer-vulkan/src.cmake
+++ src/engine/renderer-vulkan/src.cmake
@@ -3,8 +3,18 @@ set( mathList
     ${ENGINE_DIR}/renderer-vulkan/Math/NumberTypes.h
 )
 
+set( memoryList
+    ${ENGINE_DIR}/renderer-vulkan/Memory/Allocator.cpp
+    ${ENGINE_DIR}/renderer-vulkan/Memory/Allocator.h
+    ${ENGINE_DIR}/renderer-vulkan/Memory/Array.h
+    ${ENGINE_DIR}/renderer-vulkan/Memory/BitStream.cpp
+    ${ENGINE_DIR}/renderer-vulkan/Memory/BitStream.h
+    ${ENGINE_DIR}/renderer-vulkan/Memory/IteratorSeq.h
+)
+
 set( RENDERERLIST
     ${mathList}
+    ${memoryList}
     ${ENGINE_DIR}/renderer-vulkan/DispatchRawData.cpp
     ${ENGINE_DIR}/renderer-vulkan/DispatchRawData.h
     ${ENGINE_DIR}/renderer-vulkan/Shared/Timer.cpp
