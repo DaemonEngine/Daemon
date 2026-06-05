e71b6a2b94e03d54555fb69536081498fcab73df
dev@illwieckz.net <dev@illwieckz.net>
2026-06-05 16:39:32 +0200

    yokai: only run NaCl code when YOKAI_NACL_HOST is enabled

--- cmake/DaemonNacl.cmake
+++ cmake/DaemonNacl.cmake
@@ -31,7 +31,6 @@ option(USE_NACL_SAIGO "Use Saigo toolchain to build NaCl executables" OFF)
 if (YOKAI_SYSTEM_NaCl)
   # Build nexe binary.
   if(USE_NACL_SAIGO)
-    # YOKAI_NACL_ARCH_NAME is "pnacl" here, NACL_TARGET carries the architecture.
     if (NACL_TARGET STREQUAL "amd64")
       add_definitions(-DNACL_BUILD_ARCH=x86)
       add_definitions(-DNACL_BUILD_SUBARCH=64)
@@ -47,7 +46,6 @@ if (YOKAI_SYSTEM_NaCl)
     # Those defines looks to be meaningless to produce arch-independent pexe
     # with PNaCl but they must be set to anything supported by native builds.
     # This requirement looks to be a PNaCl bug.
-    # YOKAI_NACL_ARCH_NAME is "pnacl" here, NACL_TARGET is not set.
     add_definitions( -DNACL_BUILD_ARCH=x86 )
     add_definitions( -DNACL_BUILD_SUBARCH=64 )
   endif()
@@ -63,13 +61,13 @@ else()
     add_definitions( -DNACL_WINDOWS=1 -DNACL_LINUX=0 -DNACL_ANDROID=0 -DNACL_FREEBSD=0 -DNACL_OSX=0 )
   endif()
 
-  if (YOKAI_NACL_ARCH_amd64)
+  if (YOKAI_ARCH_amd64)
     add_definitions( -DNACL_BUILD_ARCH=x86 )
     add_definitions( -DNACL_BUILD_SUBARCH=64 )
-  elseif (YOKAI_NACL_ARCH_i686)
+  elseif (YOKAI_ARCH_i686)
     add_definitions( -DNACL_BUILD_ARCH=x86 )
     add_definitions( -DNACL_BUILD_SUBARCH=32 )
-  elseif (YOKAI_NACL_ARCH_armhf)
+  elseif (YOKAI_ARCH_armhf)
     add_definitions( -DNACL_BUILD_ARCH=arm )
   else()
     # NaCl does not support this architecture natively, but these defines must
